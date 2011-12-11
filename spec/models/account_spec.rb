098a9831298df6166fbfef5dc7789c2be3db4ad4
Samson Ootoowak <ootoovak@gmail.com>
2011-12-11 20:33:58 +1300

    Fix issue with balance in accounts not saving properly.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -1,22 +1,29 @@
 require 'spec_helper'
 
 describe Account do
-  before :all do
-    @account = Fabricate(:account)
-  end
+  before { @account = Fabricate(:account) }
 
   specify { @account.should be_valid }
 
   context :balance do
     specify { @account.balance_cents.should == 0 }
-    specify { @account.currency.should == 'NZD' }
-    specify { lambda { @account.balance_cents=(10) }.should raise_error }
-    specify { lambda { @account.balance=(10) }.should raise_error }
+    specify { @account.currency.should == Money.default_currency.to_s }
+    specify { expect { @account.balance_cents=(10) }.to raise_error(ArgumentError) }
+    specify { expect { @account.balance=(10) }.to raise_error(ArgumentError) }
   end
 
   context 'when updating balance' do
-    describe '#change_balance_to' do
-      context "when valid" do
+    describe '#change_balance_to(amount)' do
+      [-5, 0, 5].each do |v|
+        context "with #{v} of type #{v.class}" do
+          before(:each) do
+            @account.change_balance_to(v)
+            @account.save
+          end
+
+          specify { @account.balance.should == Money.new(v * 100) }
+          specify { @account.transactions.last.amount.should == Money.new(v * 100) }
+        end
       end
     end
 
