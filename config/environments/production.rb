209d87f561194760a46da2de9531dcc1b553218e
Samson Ootoowak <ootoovak@gmail.com>
2011-12-13 12:14:56 +1300

    Create a staging environment as well as adding mailer tweaks to each type of environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ BuckyBox::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'my.buckybox.com' }
 
   # Enable threaded mode
   # config.threadsafe!
---
+++ config/initializers/mail_safe.rb
@@ -0,0 +1,8 @@
+if defined?(MailSafe::Config)
+  MailSafe::Config.internal_address_definition = lambda { |address|
+    address =~ /.*@buckybox\.com/i ||
+    address =~ /.*@enspiral\.com/i
+  }
+
+  MailSafe::Config.replacement_address = 'samson@buckybox.com'
+end
