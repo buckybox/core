99ebce912d49812f09b31999c5d237d82ad6cbbe
Joshua Vial <joshua@enspiral.com>
2011-12-15 10:38:53 +1300

    installing analytical

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  if Rails.env == 'production'
+    analytical :modules=>[:google, :kiss_metrics], :use_session_store=>true
+  else
+    analytical :modules=>[:console], :use_session_store=>true
+  end
+
 end
