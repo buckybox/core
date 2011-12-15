657916a36101e88783918a42be624786068a8d08
Samson Ootoowak <ootoovak@gmail.com>
2011-12-15 15:56:36 +1300

    Add some customer testing to full name methods.

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,16 @@ describe Customer do
   end
 
   specify { @customer.should be_valid }
+
+  context 'full name' do
+    describe '#name' do
+      specify { @customer.name.should == "#{@customer.first_name} #{@customer.last_name}" }
+    end
+
+    describe '#name=' do
+      before { @customer.name= 'John Smith' }
+      specify { @customer.first_name.should == 'John' }
+      specify { @customer.last_name.should == 'Smith' }
+    end
+  end
 end
