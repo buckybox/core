991f694ecec1a995eb58519ef95fa714c8a31ada
Samson Ootoowak <ootoovak@gmail.com>
2011-12-17 13:40:47 +1300

    Hotfix on boxes view. Removed customer helper in favour of Money's format also fixed content_tag implementation.

--- app/helpers/distributor/boxes_helper.rb
+++ app/helpers/distributor/boxes_helper.rb
@@ -1,10 +1,6 @@
 module Distributor::BoxesHelper
-  def price_for(box)
-    "$#{box.price}"
-  end
-
   def option_status_for(option_value)
     status = option_value ? '&#x2714' : '&#10007'
-    content_tag :span, status, :class => "status status-#{option-value}"
+    content_tag :span, status.html_safe, :class => "status status-#{option_value}"
   end
 end
