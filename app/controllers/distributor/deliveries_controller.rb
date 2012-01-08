36d095862e9e19406843103540e889f1f7eef622
Samson Ootoowak <ootoovak@gmail.com>
2012-01-08 20:06:21 +1300

    A bit of a code refactor.

--- app/controllers/distributor/deliveries_controller.rb
+++ app/controllers/distributor/deliveries_controller.rb
@@ -5,32 +5,18 @@ class Distributor::DeliveriesController < Distributor::BaseController
   respond_to :html, :xml, :except => :update_status
   respond_to :json
 
+  #TODO: Pull out as much code as possible from here into a lib
+
   def index
     index! do
       start_date = Time.now - 1.week
       end_date = start_date + 15.weeks
       @selected_date = Date.parse(params['date']) if params['date']
 
-      @calendar_hash = {}
-
-      current_distributor.orders.each do |order|
-        order.schedule.occurrences_between(start_date, end_date).each do |occurrence|
-          occurrence = occurrence.to_date
-
-          @calendar_hash[occurrence] = { count: 0, order_ids: [] } if @calendar_hash[occurrence].nil?
-          @calendar_hash[occurrence][:count] += 1
-          @calendar_hash[occurrence][:order_ids] << order.id
-        end
-      end
-
+      @calendar_hash = calendar_nav_data(start_date, end_date)
       @orders = ( @selected_date ? Order.find(@calendar_hash[@selected_date][:order_ids]) : [] )
-
-      @calendar_hash = @calendar_hash.to_a.sort!
-      number_of_month_dividers = @calendar_hash.map{ |ch| ch.first.strftime("%m %Y") }.uniq.length - 1
-
-      @nav_length = @calendar_hash.length + number_of_month_dividers
-
-      @route = Route.best_route(current_distributor)
+      @calendar_hash = @calendar_hash.to_a.sort
+      @routes = current_distributor.routes
     end
   end
 
@@ -39,17 +25,42 @@ class Distributor::DeliveriesController < Distributor::BaseController
     status = params[:status]
 
     if status == 'reschedule' || status == 'pack'
-      missed_type = status
-      date = Date.parse(params[:date])
+      options = { missed_type: status, date: Date.parse(params[:date]) }
       status = 'cancelled'
     end
 
-    valid_status = Delivery::STATUS.include?(status)
-
-    if valid_status && deliveries.map{ |d| d.update_attribute('status', status) }.all?
+    if update_actions(deliveries, status, options)
       head :ok
     else
       head :bad_request
     end
+  end
+
+  private
+
+  def calendar_nav_data(start_date, end_date)
+    calendar_hash = {}
+
+    current_distributor.orders.each do |order|
+      order.schedule.occurrences_between(start_date, end_date).each do |occurrence|
+        occurrence = occurrence.to_date
+
+        calendar_hash[occurrence] = { count: 0, order_ids: [] } if calendar_hash[occurrence].nil?
+        calendar_hash[occurrence][:count] += 1
+        calendar_hash[occurrence][:order_ids] << order.id
+      end
+    end
+
+    return calendar_hash
+  end
+
+  def update_actions(deliveries, status, options = {})
+    return false if Delivery::STATUS.include?(status)
+
+    if status == 'delivered'
+
+    end
+
+    valid_status && deliveries.map{ |d| d.update_attribute('status', status) }.all?
   end
 end
--- app/helpers/distributor/deliveries_helper.rb
+++ app/helpers/distributor/deliveries_helper.rb
@@ -1,2 +1,8 @@
 module Distributor::DeliveriesHelper
+  def calendar_nav_length(calendar_hash)
+    number_of_month_dividers = calendar_hash.map{ |ch| ch.first.strftime("%m %Y") }.uniq.length - 1
+    nav_length = calendar_hash.length + number_of_month_dividers
+
+    return "#{nav_length * 59}px"
+  end
 end
