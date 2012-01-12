2fa9f549b3acbbc44075990ce78cfac9eea5c479
Samson Ootoowak <ootoovak@gmail.com>
2012-01-12 13:19:06 +1300

    Tweak to fix delivery view when no date is provided.

--- app/controllers/distributor/deliveries_controller.rb
+++ app/controllers/distributor/deliveries_controller.rb
@@ -15,7 +15,7 @@ class Distributor::DeliveriesController < Distributor::BaseController
       @calendar_hash = calendar_nav_data(start_date, end_date)
 
       if params[:view] == 'packing'
-        @orders = current_distributor.orders
+        @orders = current_distributor.orders.active
       else
         @route = (params[:view] ? current_distributor.routes.find(params[:view]) : Route.best_route(current_distributor))
 
@@ -26,13 +26,20 @@ class Distributor::DeliveriesController < Distributor::BaseController
         end
       end
 
-      unless @selected_date
-        @selected_date = @calendar_hash.keys.find { |ch_key| ch_key <= Date.today }
-        redirect_to date_distributor_deliveries_url(current_distributor, @selected_date) and return
-      end
-
       @calendar_hash = @calendar_hash.to_a.sort
       @routes = current_distributor.routes
+
+      unless @selected_date
+        @selected_date = @calendar_hash.find { |sd| puts "--- #{sd.first}"; sd.first <= Date.today }
+
+        if @selected_date
+          @selected_date = @selected_date.first
+        else
+          @selected_date = @calendar_hash.first.first
+        end
+
+        redirect_to date_distributor_deliveries_url(current_distributor, @selected_date.to_s) and return
+      end
     end
   end
 
@@ -55,7 +62,7 @@ class Distributor::DeliveriesController < Distributor::BaseController
   def calendar_nav_data(start_date, end_date)
     calendar_hash = {}
 
-    current_distributor.orders.each do |order|
+    current_distributor.orders.active.each do |order|
       order.schedule.occurrences_between(start_date, end_date).each do |occurrence|
         occurrence = occurrence.to_date
 
