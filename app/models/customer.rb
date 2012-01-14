e033176770da43eb94ff8ed9563fa34a89754fe0
Samson Ootoowak <ootoovak@gmail.com>
2012-01-15 05:28:40 +1300

    Some UI and model tweaks.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -26,9 +26,9 @@ class Customer < ActiveRecord::Base
 
   accepts_nested_attributes_for :address
 
-  attr_accessible :address_attributes, :first_name, :last_name, :email, :phone, :name, :distributor_id, :distributor
+  attr_accessible :address_attributes, :first_name, :last_name, :email, :phone, :name, :distributor_id, :distributor, :route
 
-  validates_presence_of :first_name, :last_name, :email, :distributor, :route
+  validates_presence_of :first_name, :email, :distributor, :route
   validates_uniqueness_of :email, :scope => :distributor_id
   validates_uniqueness_of :number, :scope => :distributor_id
 
