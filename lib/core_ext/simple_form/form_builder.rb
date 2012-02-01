b70b7b13ec026609496be725722a74bb2c7eecc7
Samson Ootoowak <ootoovak@gmail.com>
2012-02-01 14:13:01 +1300

    Change form builder to create flat buttons with slight rounded edge.

--- lib/core_ext/simple_form/form_builder.rb
+++ lib/core_ext/simple_form/form_builder.rb
@@ -3,7 +3,7 @@ SimpleForm::FormBuilder.class_eval do
   alias :old_button :button
   def button(type, *args, &block)
     options = args.extract_options!
-    options[:class] = "button nice #{options[:class]}".strip
+    options[:class] = "button radius #{options[:class]}".strip
     args << options
     if respond_to?("#{type}_button")
       send("#{type}_button", *args, &block)
