4779ad8f7f99cdba18417020b2a648921803622d
Samson Ootoowak <ootoovak@gmail.com>
2012-02-02 13:13:43 +1300

    Some coding style tweaks and some tweaks needed because of account-customer association changes.

--- spec/models/bank_statement_spec.rb
+++ spec/models/bank_statement_spec.rb
@@ -7,7 +7,9 @@ describe BankStatement do
     @statement = Fabricate(:bank_statement, distributor: @distributor)
     @customer = Fabricate(:customer)
     @customers_ids = {}
+
     i = 0
+
     CSV.foreach(@statement.statement_file.path, :headers => true) do |row|
       if row['Amount'].to_i > 0
         @customers_ids["#{i}"] = @customer.id.to_s
@@ -23,15 +25,16 @@ describe BankStatement do
       @statement.should_receive('create_payment!').exactly(@customers_ids.length).times
       @statement.process_statement! @customers_ids
     end
-    
+
     context :associations do
       before :each do
         @statement.save!
         hash = {"0" => "1"}
         @statement.process_statement!(hash)
       end
+
       it 'sets a statement_id to the payments' do
-        Payment.last.statement_id.should == @statement.id
+        Payment.last.bank_statement_id.should == @statement.id
       end
     end
   end
@@ -42,6 +45,7 @@ describe BankStatement do
       @statement2 = Fabricate(:bank_statement, distributor: @distributor)
       @remembers = @statement2.customer_remembers
     end
+
     specify {@remembers.should_not be_nil}
   end
 end
