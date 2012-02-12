5a1f7e0695421e9ae0bff9e560e849fd3db25851
Samson Ootoowak <ootoovak@gmail.com>
2012-02-13 09:03:35 +1300

    Starting work on tweaking specs.

--- app/models/distributor.rb
+++ app/models/distributor.rb
@@ -47,36 +47,6 @@ class Distributor < ActiveRecord::Base
   before_save :generate_daily_lists_schedule, :if => 'daily_lists_schedule.to_s.blank?'
   before_save :generate_auto_delivery_schedule, :if => 'auto_delivery_schedule.to_s.blank?'
 
-  def daily_lists_schedule
-    Schedule.from_hash(self[:daily_lists_schedule]) if self[:daily_lists_schedule]
-  end
-
-  def daily_lists_schedule=(daily_lists_schedule)
-    raise(ArgumentError, 'The daily list schedule can not be updated this way. Please use the schedule generation method.')
-  end
-
-  def generate_daily_lists_schedule(time = Time.new.beginning_of_day)
-    time = Time.at((time.to_f / 1.hour).floor * 1.hour) # make sure time starts on the hour
-    schedule = Schedule.new(time, :duration => 3600) # make sure it lasts for an hour
-    schedule.add_recurrence_rule Rule.daily # and have it reoccur daily
-    self[:daily_lists_schedule] = schedule.to_hash
-  end
-
-  def auto_delivery_schedule
-    Schedule.from_hash(self[:auto_delivery_schedule]) if self[:auto_delivery_schedule]
-  end
-
-  def auto_delivery_schedule=(auto_delivery_schedule)
-    raise(ArgumentError, 'The auto delivery schedule can not be updated this way. Please use the schedule generation method.')
-  end
-
-  def generate_auto_delivery_schedule(time = Time.new.end_of_day)
-    time = Time.at((time.to_f / 1.hour).floor * 1.hour) # make sure time starts on the hour
-    schedule = Schedule.new(time, :duration => 3600) # make sure it lasts for an hour
-    schedule.add_recurrence_rule Rule.daily # and have it reoccur daily
-    self[:auto_delivery_schedule] = schedule.to_hash
-  end
-
   def self.create_daily_lists(time = Time.now)
     logger.info "--- Checking distributor for daily list generation (#{time}) ---"
 
@@ -97,13 +67,6 @@ class Distributor < ActiveRecord::Base
     end
   end
 
-  def create_daily_lists(date = Date.current)
-    packing_list = PackingList.generate_list(self, date)
-    delivery_list = DeliveryList.generate_list(self, date)
-
-    return packing_list.persisted? && delivery_list.persisted?
-  end
-
   def self.automate_completed_status(time = Time.now)
     logger.info "--- Marking distributor daily lists as complete (#{time}) ---"
 
@@ -121,6 +84,43 @@ class Distributor < ActiveRecord::Base
         end
       end
     end
+  end
+
+  def daily_lists_schedule
+    Schedule.from_hash(self[:daily_lists_schedule]) if self[:daily_lists_schedule]
+  end
+
+  def daily_lists_schedule=(daily_lists_schedule)
+    raise(ArgumentError, 'The daily list schedule can not be updated this way. Please use the schedule generation method.')
+  end
+
+  def auto_delivery_schedule
+    Schedule.from_hash(self[:auto_delivery_schedule]) if self[:auto_delivery_schedule]
+  end
+
+  def auto_delivery_schedule=(auto_delivery_schedule)
+    raise(ArgumentError, 'The auto delivery schedule can not be updated this way. Please use the schedule generation method.')
+  end
+
+  def generate_daily_lists_schedule(time = Time.new.beginning_of_day)
+    time = Time.at((time.to_f / 1.hour).floor * 1.hour) # make sure time starts on the hour
+    schedule = Schedule.new(time, :duration => 3600) # make sure it lasts for an hour
+    schedule.add_recurrence_rule Rule.daily # and have it reoccur daily
+    self[:daily_lists_schedule] = schedule.to_hash
+  end
+
+  def generate_auto_delivery_schedule(time = Time.new.end_of_day)
+    time = Time.at((time.to_f / 1.hour).floor * 1.hour) # make sure time starts on the hour
+    schedule = Schedule.new(time, :duration => 3600) # make sure it lasts for an hour
+    schedule.add_recurrence_rule Rule.daily # and have it reoccur daily
+    self[:auto_delivery_schedule] = schedule.to_hash
+  end
+
+  def create_daily_lists(date = Date.current)
+    packing_list = PackingList.generate_list(self, date)
+    delivery_list = DeliveryList.generate_list(self, date)
+
+    return packing_list.persisted? && delivery_list.persisted?
   end
 
   def automate_completed_status(date = Date.current)
