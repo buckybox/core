01015807aefb315f2aac5a6ded599f5a3ec18337
Samson Ootoowak <ootoovak@gmail.com>
2012-02-14 13:59:14 +1300

    Fix failing customer test.

--- spec/controllers/customer/dashboard_controller_spec.rb
+++ spec/controllers/customer/dashboard_controller_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Customer::DashboardController do
+  before do
+    @customer = Fabricate(:customer)
+    sign_in @customer
+  end
 
   describe "GET 'index'" do
     it "returns http success" do
@@ -8,5 +12,4 @@ describe Customer::DashboardController do
       response.should be_success
     end
   end
-
 end
