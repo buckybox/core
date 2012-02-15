be4ccc85aaad728c82f2f03f5b98e203c6c9f30d
Samson Ootoowak <ootoovak@gmail.com>
2012-02-15 13:26:05 +1300

    Add a better home page if not logged in as distributor or customer.

--- app/controllers/bucky_box_controller.rb
+++ app/controllers/bucky_box_controller.rb
@@ -2,6 +2,5 @@ class BuckyBoxController < ApplicationController
   def index
     redirect_to distributor_root_path and return if current_distributor
     redirect_to customer_root_path and return if current_customer
-    redirect_to 'http://www.buckybox.com/'
   end
 end
