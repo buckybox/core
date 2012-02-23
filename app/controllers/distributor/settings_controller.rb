bc9e993c677f72451bee03923b40f81e3cbfc40b
Jordan Carter <jordan.d.carter@gmail.com>
2012-02-23 16:26:23 +1300

    inital start on distributor settings, links work, pages exist

---
+++ app/controllers/distributor/settings_controller.rb
@@ -0,0 +1,22 @@
+class Distributor::SettingsController < Distributor::BaseController
+
+  def index
+
+  end
+
+  def routes
+  end
+
+  def boxes
+  end
+
+  def contact_info
+  end
+
+  def bank_info
+  end
+
+  def basic_reporting
+  end
+
+end
---
+++ app/helpers/distributor/settings_helper.rb
@@ -0,0 +1,3 @@
+module Distributor::SettingsHelper
+
+end
