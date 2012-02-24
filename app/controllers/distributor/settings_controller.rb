5524ce9e9a87d326f8266e86cb032aa02e97561a
Jordan Carter <jordan.d.carter@gmail.com>
2012-02-24 14:29:41 +1300

    routes settings page done, just testing and polish

--- app/controllers/distributor/settings_controller.rb
+++ app/controllers/distributor/settings_controller.rb
@@ -1,10 +1,14 @@
 class Distributor::SettingsController < Distributor::BaseController
 
-  def index
+  respond_to :html, :json
 
+  def index
+    redirect_to action: :routes
   end
 
   def routes
+    @route = Route.new
+    @routes = current_distributor.routes
   end
 
   def boxes
