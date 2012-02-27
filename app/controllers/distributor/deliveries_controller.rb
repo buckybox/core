15324f346917444aa38728d066ff52892e47e291
Samson Ootoowak <sam@enspiral.com>
2012-02-27 20:18:14 +1300

    Hotfix CSV export issue. I had moved the phone number from customer to address. Missed one instance in this change and it was in the CSV export.

--- app/controllers/distributor/deliveries_controller.rb
+++ app/controllers/distributor/deliveries_controller.rb
@@ -99,7 +99,7 @@ class Distributor::DeliveriesController < Distributor::BaseController
             customer.id,
             customer.first_name,
             customer.last_name,
-            customer.phone,
+            address.phone,
             (delivery.customer.new? ? 'NEW' : nil),
             address.address_1,
             address.address_2,
