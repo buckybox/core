f81497db464ae04f908e8092ef5ab72f21e56ec4
Jordan Carter <jordan.d.carter@gmail.com>
2012-02-27 23:02:18 +1300

    distributor settings page for contact info done and tested

--- app/controllers/distributors_controller.rb
+++ app/controllers/distributors_controller.rb
@@ -1,4 +1,4 @@
-class DistributorsController < InheritedResources::Base
+class DistributorsController < Distributor::BaseController
   before_filter :authenticate_distributor! # just to add some sort of authentication
 
   respond_to :html, :xml, :json
@@ -6,8 +6,8 @@ class DistributorsController < InheritedResources::Base
   #TODO: this looks like it is missing authentication to me - jbv
   def update
     update! do |success, failure|
-      success.html { redirect_to distributor_wizard_boxes_url }
-      failure.html { redirect_to :back }
+      success.html { redirect_to contact_info_distributor_settings_url(@distributor) }
+      failure.html { render template: 'distributor/settings/contact_info' }
     end
   end
 end
