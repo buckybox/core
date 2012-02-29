ad4e57a9c6e8efd1497d12adfdeb9ce282b5cfeb
Samson Ootoowak <sam@enspiral.com>
2012-02-29 15:24:00 +1300

    Remove pending tests. Will just rewrite when we hit them again.

--- spec/controllers/distributor/invoices_controller_spec.rb
+++ spec/controllers/distributor/invoices_controller_spec.rb
@@ -13,17 +13,4 @@ describe Distributor::InvoicesController do
     response.should be_success
     assigns(:invoices).should_not be_empty
   end
-
-  context "to_send" do
-    it "lists invoices that need invoicing" do
-      pending
-    end
-  end
-
-  context "do_send" do
-    it "sends invoices" do
-      pending
-    end
-  end
-
 end
