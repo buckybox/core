d4e149e1ee1dd3e06fa35d72a905b2a1e2ae080b
Samson Ootoowak <sam@enspiral.com>
2012-02-29 14:22:42 +1300

    All kinds of typos. Get it togher Sam!

--- db/migrate/20120229005336_split_existing_phone_numbers.rb
+++ db/migrate/20120229005336_split_existing_phone_numbers.rb
@@ -2,16 +2,16 @@ class SplitExistingPhoneNumbers < ActiveRecord::Migration
   class Address < ActiveRecord::Base; end
 
   def up
-    Address.reset_column_informaion
+    Address.reset_column_information
 
-    Address.add.each do |address|
+    Address.all.each do |address|
       value = address.read_attribute(:phone_1)
 
       if value
         numbers = value.split('/').map(&:strip).reverse
-        address.update_attribute(:phone_1, numbers.pop) if numbers > 0
-        address.update_attribute(:phone_2, numbers.pop) if numbers > 0
-        address.update_attribute(:phone_3, numbers.pop) if numbers > 0
+        address.update_attribute(:phone_1, numbers.pop) if numbers.length > 0
+        address.update_attribute(:phone_2, numbers.pop) if numbers.length > 0
+        address.update_attribute(:phone_3, numbers.pop) if numbers.length > 0
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120228210358) do
+ActiveRecord::Schema.define(:version => 20120229005336) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "customer_id"
