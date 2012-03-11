c36f83729917402778273c30b0d0e90488a8df94
Samson Ootoowak <sam@enspiral.com>
2012-03-12 03:29:22 +1300

    Revert development env config.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,9 +15,9 @@ BuckyBox::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'buckybox.local:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'localhost', :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => 'buckybox.local', :port => 1025 }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
