6fc957987a46999e8f4820698fc6613748fd502c
Samson Ootoowak <sam@enspiral.com>
2012-03-14 11:13:57 +1300

    Fix down migration.

--- db/migrate/20120307040041_add_cron_infromation_to_distributors.rb
+++ db/migrate/20120307040041_add_cron_infromation_to_distributors.rb
@@ -1,10 +1,19 @@
 class AddCronInfromationToDistributors < ActiveRecord::Migration
-  def change
+  def up
     add_column :distributors, :advance_hour, :integer
     add_column :distributors, :advance_days, :integer
     add_column :distributors, :automatic_delivery_hour, :integer
 
     remove_column :distributors, :daily_lists_schedule
     remove_column :distributors, :auto_delivery_schedule
+  end
+
+  def down
+    add_column :distributors, :auto_delivery_schedule, :text
+    add_column :distributors, :daily_lists_schedule, :text
+
+    remove_column :distributors, :automatic_delivery_hour
+    remove_column :distributors, :advance_days
+    remove_column :distributors, :advance_hour
   end
 end
