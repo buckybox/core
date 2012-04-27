8bd804fc69bb6a1f011e752f69a6e9f29c4adf8e
Samson Ootoowak <sam@enspiral.com>
2012-04-27 13:05:56 +1200

    Updating admin view to sort Distributors in dashboard by order count.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
   def index
-    @distributors = Distributor.limit(5)
+    # OPTIMIZE: This should be done with some sort of Arel magic or something (NOT SQL) but this works for now.
+    @distributors = Distributor.all.sort { |a,b| b.orders.size <=> a.orders.size }[0..5]
     @cron_logs    = CronLog.limit(25)
   end
 end
