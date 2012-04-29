3be1e4a8879dddc5283a22a56cff563f513a3894
Samson Ootoowak <sam@enspiral.com>
2012-04-30 10:46:10 +1200

    Move frequency method in schedule and put Frequency class in it's own file.

--- lib/bucky/util.rb
+++ lib/bucky/util.rb
@@ -1,24 +1,22 @@
-module Bucky
-  # This are not used in the Bucky Box app but rather are convenience methods for development
-  module Util
-    def self.record_schedule(klass, column)
-      @recorded_schedules ||= {}
-      @recorded_schedules[klass] ||= []
-      @recorded_schedules[klass] << column
+# This are not used in the Bucky Box app but rather are convenience methods for development
+module Bucky::Util
+  def self.record_schedule(klass, column)
+    @recorded_schedules ||= {}
+    @recorded_schedules[klass] ||= []
+    @recorded_schedules[klass] << column
+  end
+
+  def self.schedules
+    # Load all models so they can initialize and set up schedules
+    (ActiveRecord::Base.connection.tables - %w[schema_migrations]).each do |table|
+      table.classify.constantize rescue nil
     end
 
-    def self.schedules
-      # Load all models so they can initialize and set up schedules
-      (ActiveRecord::Base.connection.tables - %w[schema_migrations]).each do |table|
-        table.classify.constantize rescue nil
-      end
+    @recorded_schedules.clone
+  end
 
-      @recorded_schedules.clone
-    end
-    
-    def self.fuzzy_match(a, b)
-      @@fuzzy ||= FuzzyStringMatch::JaroWinkler.create(:native)
-      @@fuzzy.getDistance(a.downcase.strip, b.downcase.strip)
-    end
+  def self.fuzzy_match(a, b)
+    @@fuzzy ||= FuzzyStringMatch::JaroWinkler.create(:native)
+    @@fuzzy.getDistance(a.downcase.strip, b.downcase.strip)
   end
 end
