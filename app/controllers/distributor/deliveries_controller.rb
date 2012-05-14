d436af6f32c3e2951b4de1e85f9a48fb658c24cc
Samson Ootoowak <sam@enspiral.com>
2012-05-14 23:34:04 +1200

    Export in the same order as possitioned.

--- app/controllers/distributor/deliveries_controller.rb
+++ app/controllers/distributor/deliveries_controller.rb
@@ -68,7 +68,7 @@ class Distributor::DeliveriesController < Distributor::ResourceController
 
     if export_type == :delivery
       export_items = current_distributor.deliveries.where(id: params[:deliveries])
-      export_items = export_items.sort_by { |ei| ei.delivery_number }
+      export_items = export_items.sort_by { |ei| ei.position }
       csv_headers = Delivery.csv_headers
     else
       export_items = current_distributor.packages.where(id: params[:packages])
