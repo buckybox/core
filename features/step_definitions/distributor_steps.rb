78b4a6506517906558a26094b2bf63bc553252ee
Craig Ambrose <craig@craigambrose.com>
2012-06-02 20:32:51 +1200

    first passing scenario

--- features/step_definitions/distributor_steps.rb
+++ features/step_definitions/distributor_steps.rb
@@ -7,12 +7,12 @@ end
 
 Given /^I am a distributor$/ do
   @distributor = Fabricate(:distributor, :password => "password", :password_confirmation => 'password')
-  @customer1 = Fabricate(:customer, :distributor => @distributor)
+  @customer = Fabricate(:customer, :distributor => @distributor)
   login_as(@distributor)
 end
 
 Given /^I am on the dashboard$/ do
-  visit "/distributors"
+  visit "/"
 end
 
 Given /^a distributor looking at their dashboard$/ do
@@ -21,7 +21,20 @@ Given /^a distributor looking at their dashboard$/ do
 end
 
 When /^I submit valid payment details$/ do
-  select @customer.badge, :from => "Customer"
+  select "##{@customer.id} #{@customer.name}", :from => "Customer"
   fill_in "Amount", :with => "127.00"
   fill_in "Description", :with => "awesome payment"
+  click_button "Create Payment"
+end
+
+Then /^the payment is recorded against that customer$/ do
+  visit distributor_customer_path(@customer)
+  save_and_open_page
+  within_table('transactions') do
+    page.should have_content('$127.00')
+  end
+end
+
+Then /^the customer balance increases by the payment amount$/ do
+  page.should have_css('#account-balance', :text => '$127.00')
 end
