5b218bc36dd4abc2f6829d852d235914e5ad1cc5
Samson Ootoowak <sam@enspiral.com>
2012-06-14 16:32:08 +1200

    Create a migration to add deductions to old deliveries and transactions that didn't have them.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120613015239) do
+ActiveRecord::Schema.define(:version => 20120614000606) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "customer_id"
