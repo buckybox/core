5ab39082e0a01765ef11442aa27f1fc42e214fce
Samson Ootoowak <sam@enspiral.com>
2012-06-28 11:27:51 +1200

    aannndd run migration again.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120614000606) do
+ActiveRecord::Schema.define(:version => 20120617101936) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "customer_id"
@@ -336,6 +336,15 @@ ActiveRecord::Schema.define(:version => 20120614000606) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "line_items", :force => true do |t|
+    t.integer  "distributor_id"
+    t.string   "name"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  add_index "line_items", ["distributor_id"], :name => "index_line_items_on_distributor_id"
 
   create_table "order_extras", :force => true do |t|
     t.integer  "order_id"
