97d80f21cebc41fe82814abe4e13f29585d505a6
Jordan Carter <jordan.d.carter@gmail.com>
2012-07-02 11:48:57 +1200

    put delivery window setting min from 0 -> 1 to fix bug in prod, need to fix some data after this too

--- app/helpers/distributor/settings_helper.rb
+++ app/helpers/distributor/settings_helper.rb
@@ -5,6 +5,6 @@ module Distributor::SettingsHelper
 
   def days_in_advance(n = 7)
     # Makes [['1 day', 1],['2 days', 2],['3 days', 3],...]
-    (0..n.to_i).to_a.map { |i| [pluralize(i, 'day'), i] }
+    (1..n.to_i).to_a.map { |i| [pluralize(i, 'day'), i] }
   end
 end
