ef72c0af68c12066f0e5dd4eb3c9f3c06ac05f9f
Jordan Carter <jordan.d.carter@gmail.com>
2012-08-21 17:06:19 +1200

    added fortnightly and monthly

--- app/models/schedule_rule.rb
+++ app/models/schedule_rule.rb
@@ -5,7 +5,7 @@ class ScheduleRule < ActiveRecord::Base
   RECUR = [:one_off, :weekly, :fortnightly, :monthly]
   
   def initalize(attrs)
-    defaults = {time_zone: Time.zone}
+    defaults = {time_zone: 'UTC'}
     super.new(defaults.merge(attrs))
   end
 
@@ -13,12 +13,26 @@ class ScheduleRule < ActiveRecord::Base
     ScheduleRule.new(start_date: date)
   end
 
-  def self.weekly(start_date, days)
+  def self.recur_on(start_date, days, recur)
     days &= DAYS #whitelist
     days = days.inject({}){|h, i| h.merge(i => true)} #Turn array into hash
     days = DAYS.inject({}){|h, i| h.merge(i => false)}.merge(days) # Fill out the rest with false
 
-    ScheduleRule.new(days.merge(recur: :weekly, start_date: start_date))
+    throw "recur '#{recur}' is :weekly or :fortnightly" unless [:weekly, :fortnightly, :monthly].include?(recur)
+
+    ScheduleRule.new(days.merge(recur: recur, start_date: start_date))
+  end
+
+  def self.weekly(start_date, days)
+    recur_on(start_date, days, :weekly)
+  end
+
+  def self.fortnightly(start_date, days)
+    recur_on(start_date, days, :fortnightly)
+  end
+
+  def self.monthly(start_date, days)
+    recur_on(start_date, days, :monthly)
   end
 
   def recur
@@ -46,5 +60,15 @@ class ScheduleRule < ActiveRecord::Base
   def weekly_occurs_on?(date)
     day = date.strftime("%a").downcase.to_sym
     self.send("#{day}?") && start_date <= date
+  end
+
+  def fortnightly_occurs_on?(date)
+    # So, theory is, the difference between the start date and the date in question as days, devided by 7 should give the number of weeks since the start date.  If the number is even, we are on a fortnightly.
+    first_occurence = start_date + (date.wday - start_date.wday) + (start_date.wday >= date.wday ? 7 : 0)
+    weekly_occurs_on?(date) && ((date - first_occurence) / 7).to_i.even? # 7 days in a week
+  end
+
+  def monthly_occurs_on?(date)
+    weekly_occurs_on?(date) && date.day < 8
   end
 end
