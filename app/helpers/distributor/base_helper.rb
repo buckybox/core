631ed1d6518ec729aa6932336d854d19c8d0b713
Samson Ootoowak <sam@enspiral.com>
2012-08-31 13:09:08 +1200

    Sort out site navigation html

--- app/helpers/distributor/base_helper.rb
+++ app/helpers/distributor/base_helper.rb
@@ -11,19 +11,6 @@ module Distributor::BaseHelper
     return address.join(join_with).html_safe
   end
 
-  def topic_tab(text, path, options = {})
-    css_class = (current_page?(path) ? 'active main' : 'main')
-    is_image = options.delete(:image_link)
-
-    content_tag :li do
-      if is_image
-        link_to image_tag(text), path, options.merge(class: css_class)
-      else
-        link_to text, path, options.merge(class: css_class)
-      end
-    end
-  end
-
   # force_highlighted = :routes
   # will make the 'Routes' sub_nav title show as highlighted
   def show_settings_sub_nav(force_highlighted = nil)
