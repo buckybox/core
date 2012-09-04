58936ed1048619471bdc43b60a73fab2c54436b8
Jordan Carter <jordan.d.carter@gmail.com>
2012-09-04 17:30:42 +1200

    migration to upcase the currency code for distributors

---
+++ db/migrate/20120904052651_update_distributor_currency_to_upcase.rb
@@ -0,0 +1,14 @@
+class UpdateDistributorCurrencyToUpcase < ActiveRecord::Migration
+  def up
+    Distributor.reset_column_information
+    Distributor.transaction do
+      Distributor.where(id: [14, 15]).all.each do |d|
+        d.currency = d.currency.upcase
+        d.save!
+      end
+    end
+  end
+
+  def down
+  end
+end
