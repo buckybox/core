43b548afe6478ea0cb2f082dd36e6a7c61196bd0
Jordan Carter <jordan.d.carter@gmail.com>
2012-09-13 18:04:22 +1200

    touching migrations is bad, unless it changes data for production??

--- db/migrate/20120904051101_update_distributor_country.rb
+++ db/migrate/20120904051101_update_distributor_country.rb
@@ -2,7 +2,7 @@ class UpdateDistributorCountry < ActiveRecord::Migration
   def up
     Distributor.reset_column_information
     Distributor.transaction do
-      [[2, 'NZ'], [14, 'AU'], [15, "NZ"]].each do |id, c_code|
+      [[20, 'NZ'],[7, 'NZ'],[19, 'Mexico'], [2, 'NZ'], [14, 'AU'], [15, "NZ"]].each do |id, c_code|
         distributor = Distributor.find_by_id(id)
         country = Country.find_by_name(c_code)
         if distributor && country
