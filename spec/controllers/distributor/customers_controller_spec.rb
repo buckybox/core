f336a93bb58d2f9280e5d6a9675435f837f5020a
Jordan Carter <jordan.d.carter@gmail.com>
2012-09-18 12:28:11 +1200

    fix customer update bug

--- spec/controllers/distributor/customers_controller_spec.rb
+++ spec/controllers/distributor/customers_controller_spec.rb
@@ -21,4 +21,15 @@ describe Distributor::CustomersController do
       response.should redirect_to(distributor_customer_path(@customer))
     end
   end
+
+  context "#update" do
+    before do
+      @customer_2 = Fabricate(:customer, distributor: @distributor, email: "duplicate@dups.com")
+    end
+
+    it 'should show the errors' do
+      put :update, id: @customer.id, customer: {email: "duplicate@dups.com"}
+      assigns(:form_type).should eq('personal_form')
+    end
+  end
 end
