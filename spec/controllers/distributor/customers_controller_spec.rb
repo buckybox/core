d36b7484f0d4b40fc6f4077b420192bfcc259e6b
Jordan Carter <jordan.d.carter@gmail.com>
2012-09-27 14:53:25 +1200

    fix test

--- spec/controllers/distributor/customers_controller_spec.rb
+++ spec/controllers/distributor/customers_controller_spec.rb
@@ -22,8 +22,20 @@ describe Distributor::CustomersController do
         response.should redirect_to(distributor_customer_path(@customer))
       end
     end
+    
+    context "#update" do
+      before do
+        @customer_2 = Fabricate(:customer, distributor: @distributor, email: "duplicate@dups.com")
+      end
+
+      it 'should show the errors' do
+        put :update, id: @customer.id, customer: {email: "duplicate@dups.com"}
+        assigns(:form_type).should eq('personal_form')
+      end
+    end
   end
 
+  
   context "performance" do
     before do
       3.times do
@@ -39,14 +51,4 @@ describe Distributor::CustomersController do
     end
   end
 
-  context "#update" do
-    before do
-      @customer_2 = Fabricate(:customer, distributor: @distributor, email: "duplicate@dups.com")
-    end
-
-    it 'should show the errors' do
-      put :update, id: @customer.id, customer: {email: "duplicate@dups.com"}
-      assigns(:form_type).should eq('personal_form')
-    end
-  end
 end
