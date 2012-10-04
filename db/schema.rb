820282f86ae3e71859ef84487ffedebd09ff9e18
Samson Ootoowak <sam@enspiral.com>
2012-10-04 22:27:18 +1300

    Tweak the deliver view a bit more.

--- db/schema.rb
+++ db/schema.rb
@@ -134,11 +134,11 @@ ActiveRecord::Schema.define(:version => 20120927224520) do
     t.string   "last_name"
     t.integer  "distributor_id"
     t.integer  "route_id"
-    t.string   "encrypted_password",       :limit => 128, :default => "",  :null => false
+    t.string   "encrypted_password",         :limit => 128, :default => "",  :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                           :default => 0
+    t.integer  "sign_in_count",                             :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -147,14 +147,16 @@ ActiveRecord::Schema.define(:version => 20120927224520) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.integer  "failed_attempts",                         :default => 0
+    t.integer  "failed_attempts",                           :default => 0
     t.string   "unlock_token"
     t.datetime "locked_at"
     t.string   "authentication_token"
-    t.decimal  "discount",                                :default => 0.0, :null => false
+    t.decimal  "discount",                                  :default => 0.0, :null => false
     t.integer  "number"
     t.text     "notes"
     t.text     "special_order_preference"
+    t.integer  "next_order_id"
+    t.date     "next_order_occurrence_date"
   end
 
   add_index "customers", ["authentication_token"], :name => "index_customers_on_authentication_token", :unique => true
