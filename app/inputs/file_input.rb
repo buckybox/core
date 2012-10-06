a57d12b9e61c8bff12ea3999eb79df6cdd3cccb3
Samson Ootoowak <sam@enspiral.com>
2012-10-06 15:27:28 +1300

    Port over the simple setting screens to bootstrap.

--- app/inputs/file_input.rb
+++ app/inputs/file_input.rb
@@ -1,7 +1,7 @@
 class FileInput < SimpleForm::Inputs::FileInput
    def input
      "<div class=\"file-input\">
-       <span class=\"btn btn-primary\">#{input_html_options[:value] || input_html_options[:label] || "Browse..."}</span>
+       <span class=\"btn\">#{input_html_options[:value] || input_html_options[:label] || "Browse..."}</span>
        <span class=\"description\"></span>
      </div>#{super}".html_safe
    end
