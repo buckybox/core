ce4e716f4bc998729c92b2d201a3911ae2e01e86
Samson Ootoowak <sam@enspiral.com>
2012-10-08 17:45:18 +1300

    Get stock item settings styled and JS working again.

--- app/assets/javascripts/old/distributor/line_items.js
+++ app/assets/javascripts/old/distributor/line_items.js
@@ -3,22 +3,28 @@ $(function() {
     $('#bulk-update').submit();
   });
 
-  $('.line-item input').keyup(function() {
+  $('#bulk-update input').keyup(function() {
     stock_item_input(this);
   });
 
-  $('.line-item .remove').click(function() {
-    input_element = $(this).closest('.line-item').find('input')[0];
+  $('#bulk-update .remove').click(function() {
+    input_element = $(this).closest('tr').find('input')[0];
     $(input_element).val('');
     stock_item_input(input_element);
+    return false;
   });
 });
 
 function stock_item_input(element) {
-  warning        = $(element).closest('.line-item').find('.warning');
-  remove         = $(element).closest('.line-item').find('.remove');
+  warning        = $(element).closest('tr').find('.label-warning');
+  remove         = $(element).closest('tr').find('.remove');
   original_value = $(element).data('original-value');
   current_value  = $(element).val();
+
+  console.info(warning);
+  console.info(remove);
+  console.info(original_value);
+  console.info(current_value);
 
   if(current_value != original_value) {
     warning.show();
