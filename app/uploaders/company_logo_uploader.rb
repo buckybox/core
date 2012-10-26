931bf6b8dd5f01934b650520c031c0401154115e
Samson Ootoowak <sam@enspiral.com>
2012-10-27 12:50:00 +1300

    Tweak on banner for customer login screen.

--- app/uploaders/company_logo_uploader.rb
+++ app/uploaders/company_logo_uploader.rb
@@ -5,7 +5,7 @@ class CompanyLogoUploader < BuckyImageUploader
     process resize_to_fit: [980, 160]
   end
 
-  version :half_size_banner, from_version: :banner do
-    process resize_to_fit: [490, 80]
+  version :frame_banner, from_version: :banner do
+    process resize_to_fit: [570, 128]
   end
 end
