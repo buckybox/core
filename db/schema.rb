a7df362b473c4cbca1bb48a7ad6b224f331ca0c6
Samson Ootoowak <sam@enspiral.com>
2012-10-31 16:09:53 +1300

    Changes after pulling production db and running migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -266,11 +266,11 @@ ActiveRecord::Schema.define(:version => 20121024025935) do
     t.integer  "country_id"
     t.integer  "consumer_delivery_fee_cents"
     t.boolean  "active_webstore",                            :default => false, :null => false
+    t.string   "city"
     t.string   "company_team_image"
     t.text     "about"
     t.text     "details"
     t.string   "facebook_url"
-    t.string   "city"
   end
 
   add_index "distributors", ["authentication_token"], :name => "index_distributors_on_authentication_token", :unique => true
