da6e5044ad44d9b498aa9854d54a970739fdc14f
Jordan Carter <jordan.d.carter@gmail.com>
2012-12-13 10:16:09 +1300

    update mail safe whitelist to only buckybox

--- config/initializers/mail_safe.rb
+++ config/initializers/mail_safe.rb
@@ -1,7 +1,6 @@
 if defined?(MailSafe::Config)
   MailSafe::Config.internal_address_definition = lambda { |address|
-    address =~ /.*@buckybox\.com/i ||
-    address =~ /.*@enspiral\.com/i
+    address =~ /.*@buckybox\.com/i
   }
 
   MailSafe::Config.replacement_address = 'beta@buckybox.com'
