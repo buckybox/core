c18e02222f033278a2e6a58abb4c7975d8a968c1
Jordan Carter <jordan.d.carter@gmail.com>
2013-01-16 15:48:47 +1300

    don't send emails to support@buckybox.com from staging

--- config/initializers/mail_safe.rb
+++ config/initializers/mail_safe.rb
@@ -1,7 +1,9 @@
 if defined?(MailSafe::Config)
   MailSafe::Config.internal_address_definition = lambda { |address|
     address =~ /.*@buckybox\.com/i ||
-    address =~ /.*@enspiral\.com/i
+    address =~ /.*@enspiral\.com/i ||
+    address == 'support@buckybox.com'
+
   }
 
   MailSafe::Config.replacement_address = 'beta@buckybox.com'
