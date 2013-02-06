5fdf810122ba3ff71e22d7569eb2a02e2dd4ae32
Jordan Carter <jordan.d.carter@gmail.com>
2013-02-06 14:13:32 +1300

    redirect to edit page for show box

--- app/controllers/distributor/boxes_controller.rb
+++ app/controllers/distributor/boxes_controller.rb
@@ -13,6 +13,11 @@ class Distributor::BoxesController < Distributor::ResourceController
     update! { distributor_settings_boxes_url }
   end
 
+  # No show page, so redirect to edit
+  def show
+    redirect_to edit_distributor_box_path(params[:id])
+  end
+
   def extras
     account = current_distributor.accounts.find_by_id(params[:account_id])
     order = Order.new
