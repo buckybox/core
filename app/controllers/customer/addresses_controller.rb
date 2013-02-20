d174afc76481774acb83fc72ae23acf636f3fab0
Jordan Carter <jordan.d.carter@gmail.com>
2013-02-21 11:24:40 +1300

    warn customers after updating address when it will take place

--- app/controllers/customer/addresses_controller.rb
+++ app/controllers/customer/addresses_controller.rb
@@ -5,9 +5,9 @@ class Customer::AddressesController < Customer::ResourceController
 
     if @address.update_attributes(params[:address])
       if current_customer.has_yellow_deliveries?
-        flash.now[:notice] = "WARNING: Your delivery address has been updated, but you have an impending delivery that is too late to change. Your address change will take effect on #{current_customer.distributor.beginning_of_green_zone.to_s(:day_date_month)}"
+        flash.now[:alert] = "WARNING: Your delivery address has been updated, but you have an impending delivery that is too late to change. Your address change will take effect on #{current_customer.distributor.beginning_of_green_zone.to_s(:day_date_month)}."
       else
-        flash.now[:notice] = "Your delivery address has been updated, this will take effect on your next delivery"
+        flash.now[:notice] = "Your delivery address has been updated, this will take effect on your next delivery."
       end
     end
   end
