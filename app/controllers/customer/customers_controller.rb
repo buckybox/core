d864b5ef95b52adc0c13b16ecd7fbea960e5061e
Jordan Carter <jordan.d.carter@gmail.com>
2013-03-04 17:50:55 +1300

    remove password changes

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -11,8 +11,6 @@ class Customer::CustomersController < Customer::ResourceController
   end
 
   def update_password
-    params[:customer][:password].strip! rescue nil #Customers copy and paste password from email, sometimes grabbing a bit of whitespace
-    params[:customer][:password_confirmation].strip! rescue nil
     respond_to do |format|
       if current_customer.update_attributes(params[:customer])
         sign_in current_customer, bypass: true
