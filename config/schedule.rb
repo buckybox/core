3b8b4b039f526f19a8a185a01354133e91377c64
Jordan Carter <jordan.d.carter@gmail.com>
2013-03-06 19:30:52 +1300

    add metrics daily job to cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,10 @@
 set :output, { :error => 'log/cron_error.log', :standard => 'log/cron.log' }
 
 every '0 * * * *' do
-  runner 'Jobs.run_all'
+  runner 'Jobs.run_hourly'
+end
+every '0 1 * * *' do
+  runner 'Jobs.run_daily'
 end
 
 #TODO we are not doing invoicing at the moment
