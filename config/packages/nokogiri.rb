04caf3a2818d345898da4bdd04b6961b176adc8d
Jordan Carter <jordan.d.carter@gmail.com>
2013-05-06 13:57:31 +1200

    update sprinkle install script

--- config/packages/nokogiri.rb
+++ config/packages/nokogiri.rb
@@ -1,13 +1,13 @@
 package :nokogiri do
   describe 'Nokogiri'
 
-  apt 'libxml2 libxml2-dev libxslt libxslt-dev' do
+  apt 'libxml2 libxml2-dev libxslt1-dev' do
     pre :install, 'apt-get -y update'
   end
   
   verify do
     has_apt 'libxml2'
     has_apt 'libxml2-dev'
-    has_apt 'libxslt-dev'
+    has_apt 'libxslt1-dev'
   end
 end
