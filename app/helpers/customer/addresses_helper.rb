7a132890c3d49d0adcbde3741b7365eae0a4e1f7
Cédric Félizard <cedric@felizard.fr>
2013-05-17 05:42:18 +0000

    Refactor to avoid hardcoded phone types

--- app/helpers/customer/addresses_helper.rb
+++ app/helpers/customer/addresses_helper.rb
@@ -1,7 +1,7 @@
 module Customer::AddressesHelper
   def address_phone_fields(form, distributor)
-    form.input(:mobile_phone, required: distributor.require_phone) <<
-    form.input(:home_phone, required: distributor.require_phone) <<
-    form.input(:work_phone, required: distributor.require_phone)
+    Address::PhoneCollection.attributes.map do |type|
+      form.input type, required: distributor.require_phone
+    end.join.html_safe
   end
 end
