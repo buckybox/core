d56049264df690361b96e4895ae0b7ca75d0e8ed
Cédric Félizard <cedric@felizard.fr>
2013-05-19 23:37:18 +0000

    Remove Kiss Metrics tracking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   unless Rails.env.development?
-    analytical modules: [:google, :kiss_metrics], use_session_store: true
+    analytical modules: [:google], use_session_store: true
   else
     analytical modules: [], use_session_store: true
   end
