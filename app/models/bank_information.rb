68beeb883b0701df673bf682d7c676f181289f8d
Cédric Félizard <cedric@felizard.fr>
2013-05-20 04:22:09 +0000

    Track all events required for the 'activated' macro

--- app/models/bank_information.rb
+++ app/models/bank_information.rb
@@ -4,4 +4,10 @@ class BankInformation < ActiveRecord::Base
   attr_accessible :distributor, :name, :account_name, :account_number, :customer_message, :bsb_number, :cod_payment_message
 
   validates_presence_of :distributor, :name, :account_name, :account_number, :customer_message, :bsb_number
+
+  after_save :usercycle_tracking
+
+  def usercycle_tracking
+    Bucky::Usercycle.instance.event(distributor.id, "distributor_populated_bank_information")
+  end
 end
