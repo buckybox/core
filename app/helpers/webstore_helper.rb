a8250c5f5b78b3d3e5dc957191e0f4e7bedf6c4a
Cédric Félizard <cedric@felizard.fr>
2013-05-22 04:20:21 +0000

    Abbreviate types so they're not truncated in dropdowns

--- app/helpers/webstore_helper.rb
+++ app/helpers/webstore_helper.rb
@@ -2,7 +2,7 @@ module WebstoreHelper
   def phone_types_collection
     Address::PhoneCollection::TYPES.each_key.map do |type|
       [
-        "#{type.capitalize} phone",
+        type.capitalize,
         type
       ]
     end
