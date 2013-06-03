af149791d48ddc16580976aeed05af3b054ff514
Samson Ootoovak <sam@enspiral.com>
2013-06-03 19:28:30 +1200

    Remove pending tests that have been unused for a long time providing no value and just getting in the way. If you really want them fix them or we can look back in the git history for them.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -12,41 +12,6 @@ describe Invoice do
     end
   end
 
-  describe "#generate_invoices" do
-    before(:each) do
-      pending('Invoicing email generation is turned off for now')
-      @account_due_today = Fabricaate(:active_recurring_order).account
-      @account_due_today.stub(:next_invoice_date).and_return(Date.current)
-      @account_due_tomorrow = Fabricaate(:active_recurring_order).account
-      @account_due_tomorrow.stub(:next_invoice_date).and_return(1.day.from_now)
-      @account_due_yesterday = Fabricaate(:active_recurring_order).account 
-      @account_due_yesterday.stub(:next_invoice_date).and_return(1.day.ago)
-      @account_with_invoice = Fabricaate(:active_recurring_order).account
-      @account_with_invoice.stub(:next_invoice_date).and_return(Date.current)
-      @account_with_invoice.create_invoice
-      Account.stub(:all).and_return([@account_due_today,@account_due_tomorrow, @account_due_yesterday, @account_with_invoice])
-      ActionMailer::Base.deliveries = []
-      @invoices = Invoice.generate_invoices
-      @invoiced_accounts = @invoices.collect {|i| i.account}
-    end
-
-    it "emails invoices to customers" do
-      ActionMailer::Base.deliveries.size.should == @invoices.size
-    end
-
-    it "creates invoices for accounts that have an invoice date before today" do
-      @invoiced_accounts.should include(@account_due_today, @account_due_yesterday)
-    end
-
-    it "does not create invoices for accounts due after today"  do
-      @invoiced_accounts.should_not include(@account_due_tomorrow)
-    end
-
-    it "does not create invoices for accounts which have an outstanding invoice already"  do
-      @invoiced_accounts.should_not include(@account_with_invoice)
-    end
-  end
-
   describe "calculate_amount" do
     context "with one scheduled delivery" do
       before(:each) do
@@ -55,11 +20,6 @@ describe Invoice do
         @account.stub(:all_occurrences).and_return([{:price => @order.box.price}])
         @invoice = Fabricate(:invoice, :account => @account)
         @invoice.calculate_amount
-      end
-
-      it "should calculate correct amount" do
-        pending 'Invoicing email generation is turned off for now'
-        @invoice.amount.should == @account.amount_with_bucky_fee(@order.box.price)
       end
     end
 
@@ -87,10 +47,6 @@ describe Invoice do
 
       it "should save deliveries hash" do
         @invoice.deliveries.size.should == @account.all_occurrences(4.weeks.from_now).size
-      end
-
-      xit "should include transactions on last day" do
-        @invoice.transactions.size.should == 2
       end
     end
   end
