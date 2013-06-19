7a81147c0a9d8b8bce41d9509856cf70df100730
Jordan Carter <jordan.d.carter@gmail.com>
2013-06-19 19:04:41 +1200

    ffs Jordan, get it right

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,7 +4,7 @@ class AdminMailer < ActionMailer::Base
     @email = email
     @admin = admin
 
-    headers['X-MC-Tags'] = "admin,preview,#{@admin.email.parameterize}"
+    headers['X-MC-Tags'] = "admin,preview,#{@email.preview_email.parameterize}"
 
     mail to: @email.preview_email,
          from: Figaro.env.support_email,
