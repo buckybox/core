baa1cc4cf926db088b13247e23a7494aaf781aca
Cédric Félizard <cedric@felizard.fr>
2013-07-08 06:53:49 +0000

    Add missing specs for sign up wizard controller

--- spec/controllers/sign_up_wizard_controller_spec.rb
+++ spec/controllers/sign_up_wizard_controller_spec.rb
@@ -5,11 +5,39 @@ describe SignUpWizardController do
     Fabricate(:country, alpha2: "NZ")
   end
 
-  let(:form_params) do
-    {"distributor"=>{"name"=>"My new org", "contact_name"=>"Name", "email"=>"email@example.net", "password"=>"password", "password_confirmation"=>"password", "country"=>"NZ", "time_zone"=>"Auckland", "localised_address_attributes"=>{"street"=>"Street", "state"=>"State", "city"=>"City", "zip"=>"Zip"}, "phone"=>"000", "support_email"=>"support@example.net", "parameter_name"=>"my-new-org", "payment_bank_deposit"=>"1", "bank_name"=>"Kiwibank", "payment_cash_on_delivery"=>"0", "payment_credit_card"=>"1", "payment_direct_debit"=>"0", "source"=>"Word of mouth"}}
+  describe "#form" do
+    before do
+      get :form
+    end
+
+    it "sets the country" do
+      assigns[:country].should eq "NZ"
+    end
+
+    it "sets the time zone" do
+      assigns[:time_zone].should eq "Pacific/Auckland"
+    end
+  end
+
+  describe "#country" do
+    before do
+      post :country, country: "NZ"
+    end
+
+    it "sets the form fields" do
+      assigns[:fields].should include(*%w(street state city zip))
+    end
+
+    it "sets the bank list" do
+      assigns[:banks].should eq ["Other"]
+    end
   end
 
   describe "#sign_up" do
+    let(:form_params) do
+      {"distributor"=>{"name"=>"My new org", "contact_name"=>"Name", "email"=>"email@example.net", "password"=>"password", "password_confirmation"=>"password", "country"=>"NZ", "time_zone"=>"Auckland", "localised_address_attributes"=>{"street"=>"Street", "state"=>"State", "city"=>"City", "zip"=>"Zip"}, "phone"=>"000", "support_email"=>"support@example.net", "parameter_name"=>"my-new-org", "payment_bank_deposit"=>"1", "bank_name"=>"Kiwibank", "payment_cash_on_delivery"=>"0", "payment_credit_card"=>"1", "payment_direct_debit"=>"0", "source"=>"Word of mouth"}}
+    end
+
     context "with valid params" do
       let(:post_form) do
         lambda { post :sign_up, form_params }
