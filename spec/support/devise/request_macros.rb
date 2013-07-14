1f32a79240d969a1ddbf10830988ec73c3532cc8
Samson Ootoovak <sam@enspiral.com>
2013-07-14 17:34:56 +1200

    Adding RSpec and Capybara integration testing.

--- spec/support/devise/request_macros.rb
+++ spec/support/devise/request_macros.rb
@@ -1,4 +1,12 @@
 module Devise::RequestMacros
+  def simulate_distributor_sign_in
+    before { distributor_sign_in }
+  end
+
+  def simulate_customer_sign_in
+    before { customer_sign_in }
+  end
+
   def as_distributor
     before { sign_in_as_a_valid_distributor }
   end
