69f906c99f7f26f227893056e40ec986fa85f2ce
Cédric Félizard <cedric@felizard.fr>
2013-07-15 03:25:21 +0000

    New wizard not using iframe so it works cross-domain

--- app/assets/javascripts/sign_up_wizard/sign_up_wizard.js
+++ app/assets/javascripts/sign_up_wizard/sign_up_wizard.js
@@ -1,6 +1,6 @@
 var BuckyBoxSignUpWizard = function() {
   this.id = "bucky_box_sign_up_wizard"
-  this.host = "//staging.buckybox.com"
+  this.host = "//staging.buckybox.com" // FIXME
 
   this.push = function() {
     for (var i = 0; i < arguments.length; i++) {
@@ -16,228 +16,263 @@ var BuckyBoxSignUpWizard = function() {
   };
 
   this.show = function() {
-    $("<iframe />", {
-      name: this.id,
-      id: this.id,
-      src: this.host + "/sign_up_wizard/form",
-      frameborder: "0",
-      style: "display: block; height: 100%; position: fixed; top: 5%; left: 50%; width: 100%; margin-left: -310px;",
-      "data-id": this.id,
-      "data-host": this.host
-    }).load(function() {
-      // iframe loaded
-      var iframe = this;
+    console.log("SHOW");
 
-      // redefine jQuery selector to select within iframe
-      var $ = function(selector) {
-        return jQuery(iframe).contents().find(selector);
-      };
+    var host = this.host,
+          id = this.id;
 
-      var id = this.getAttribute("data-id");
-      var host = this.getAttribute("data-host");
+    var css = host + "/assets/sign_up_wizard.css";
 
-      var visible_step = $(".step:visible");
-      var current_step = visible_step;
+    $.ajax({
+      url: css,
+      // dataType: "text",
+      error: function() {
+        console.log("Could not load CSS");
+      },
+      success: function() {
+        console.log("CSS loaded");
+        // Could be nicer but have to use that way for IE compat
+        $("head").append('<link rel="stylesheet" type="text/css" href="' + css + '" />');
 
-      // register event handlers
-      $("#close").click(function() {
-        alert("Sorry buddy, you shall not quit now!"); // FIXME
-      });
+        $.get(host + "/sign_up_wizard/form", function(data) {
+          $("<div />", {
+            id: id,
+            name: id,
+            frameborder: 0
+          }).html(data).appendTo("body");
 
-      $("#distributor_country").change(function() {
-        update_country();
-      });
+          console.log("LOADED");
 
-      var url_prefix = "my.buckybox.com/webstore/";
+          // jQuery selector within our div
+          var sign_up_wizard = function(selector) {
+            return $("#" + id + " " + selector);
+          };
 
-      // copy parameterzed text into URL field
-      $("#distributor_name").change(function() {
-        var slug = $(this).val().
-          replace(/\s/g, '-').replace(/[^a-zA-Z0-9\-]/g, '').
-          toLowerCase();
+          var visible_step = sign_up_wizard(".step:visible");
+          var current_step = visible_step;
 
-        $("#distributor_parameter_name").val(url_prefix + slug);
-      });
+          // register event handlers
+          sign_up_wizard("#close").click(function() {
+            // ga('send', 'event', 'sign_up_wizard', 'close'); FIXME
+            sign_up_wizard("").remove();
+          });
 
-      $("#distributor_parameter_name").on("blur", function() {
-        if ($(this).val().indexOf(url_prefix) !== 0) {
-          $(this).val(url_prefix + $(this).val());
-        }
-      });
+          sign_up_wizard("#distributor_country").change(function() {
+            update_country();
+          });
 
-      // toggle bank deposit details visibility
-      $("#distributor_payment_bank_deposit").change(function() {
-        if ($("#distributor_bank_name option").length <= 1) {
-          return false;
-        }
+          var url_prefix = "my.buckybox.com/webstore/";
 
-        // HTML5 validation don't happen on disabled fields
-        $("#bank_deposit_details :input").prop("disabled", !this.checked);
+          // copy parameterzed text into URL field
+          sign_up_wizard("#distributor_name").change(function() {
+            var slug = $(this).val().
+              replace(/\s/g, '-').replace(/[^a-zA-Z0-9\-]/g, '').
+              toLowerCase();
 
-        $("#bank_deposit_details").toggle(this.checked);
-      });
+            sign_up_wizard("#distributor_parameter_name").val(url_prefix + slug);
+          });
 
-      // turn those dropdowns into neat custom comboboxes
-      $("#distributor_bank_name").combobox();
-      $("#distributor_source").combobox();
+          sign_up_wizard("#distributor_parameter_name").on("blur", function() {
+            if ($(this).val().indexOf(url_prefix) !== 0) {
+              $(this).val(url_prefix + $(this).val());
+            }
+          });
 
-      var update_country = function() {
-        jQuery.ajax({
-          type: "GET",
-          url: host + "/sign_up_wizard/country",
-          crossDomain: true,
-          xhrFields: {
-            withCredentials: true
-          },
-          data: "country=" + $("#distributor_country").val(),
-          success: function(response) {
-            // update address fields
-            $("#address_fields").html(response.address).find("div").each(function() {
-              var inputs = $(this).find("input");
-              inputs.css("width", (420 - 7 * (inputs.length - 1)) / inputs.length);
-            });
+          // toggle bank deposit details visibility
+          sign_up_wizard("#distributor_payment_bank_deposit").change(function() {
+            if (sign_up_wizard("#distributor_bank_name option").length <= 1) {
+              return false;
+            }
 
-            // hide bank deposit details
-            $("#bank_deposit_details").hide();
-            $("#bank_deposit_details :input").prop("disabled", true);
+            // HTML5 validation don't happen on disabled fields
+            sign_up_wizard("#bank_deposit_details :input").prop("disabled", !this.checked);
 
-            // populate bank list
-            $("#distributor_bank_name").html("");
-            jQuery.each(response.banks, function() {
-              $("#distributor_bank_name").append(jQuery("<option />").val(this).text(this));
-            });
+            sign_up_wizard("#bank_deposit_details").toggle(this.checked);
+          });
 
-            register_validity_handlers();
-          }
-        });
-      };
+          // turn those dropdowns into neat custom comboboxes
+          sign_up_wizard("#distributor_bank_name").combobox();
+          sign_up_wizard("#distributor_source").combobox();
 
-      update_country();
-
-      var is_last_step = function() {
-        return (visible_step[0] == $(".step").last()[0]);
-      };
-
-      var is_first_step = function() {
-        return (visible_step[0] == $(".step").first()[0]);
-      };
-
-      // update button texts
-      var update_buttons = function() {
-        $("#back").toggle(!is_first_step());
-        $("#next").val(is_last_step() ? "Done!" : "Next");
-      };
-
-      var update_step_counter = function() {
-        var steps = $(".step");
-        $("#step").html(1 + steps.index(visible_step) + "/" + steps.length);
-      };
-
-      var change_step = function(direction) {
-        var step = $(".step:visible").hide();
-        step = (direction == 1 ? step.next() : step.prev());
-
-        step.show(0, function() {
-          current_step = step;
-        });
-
-        visible_step = step;
-
-        update_step_counter();
-        update_buttons();
-      };
-
-      // register navigation handlers
-      $("#next").click(function(e) {
-        var valid = true;
-
-        $(".step:visible input").each(function() {
-          if (!this.checkValidity()) {
-            valid = false;
-            return false;
-          }
-        });
-
-        if (valid) {
-          if (is_last_step()) {
-            var form = $('form');
-
-            jQuery.ajax({
-              type: "POST",
-              url: host + "/sign_up_wizard/sign_up",
+          var update_country = function() {
+            $.ajax({
+              type: "GET",
+              url: host + "/sign_up_wizard/country",
               crossDomain: true,
               xhrFields: {
                 withCredentials: true
               },
-              data: form.serialize(),
-              beforeSend: function() {
-                $("footer input").attr("disabled", true);
-                $("#next").val("...");
-
-                $("#message").hide();
-              },
-              complete: function() {
-                $("footer input").attr("disabled", false);
-                $("#next").val("Done!");
-              },
+              data: "country=" + sign_up_wizard("#distributor_country").val(),
               success: function(response) {
-                alert("You will receive an email with instructions soon!");
+                // update address fields
+                sign_up_wizard("#address_fields").html(response.address).find("div").each(function() {
+                  var inputs = $(this).find("input");
+                  inputs.css("width", (420 - 7 * (inputs.length - 1)) / inputs.length);
+                });
 
-                jQuery("#" + id).remove();
-              },
-              error: function(response) {
-                $("#message").html(response.responseText).show();
+                // hide bank deposit details
+                sign_up_wizard("#bank_deposit_details").hide();
+                sign_up_wizard("#bank_deposit_details :input").prop("disabled", true);
+
+                // populate bank list
+                sign_up_wizard("#distributor_bank_name").html("");
+                $.each(response.banks, function() {
+                  sign_up_wizard("#distributor_bank_name").append($("<option />").val(this).text(this));
+                });
+
+                register_validity_handlers();
               }
             });
-          } else {
-            change_step(1);
-          }
-        }
-        // else render HTML5 validation messages
-      });
+          };
 
-      $("#back").click(function() {
-        change_step(-1);
-        $("#message").hide();
-      });
+          update_country();
 
-      // register validation handlers
-      var password = $("#distributor_password");
-      var password_confirmation = $("#distributor_password_confirmation");
+          var is_first_step = function() {
+            return (visible_step[0] == sign_up_wizard(".step").first()[0]);
+          };
 
-      var checkPasswordValidity = function() {
-          if (password.val() != password_confirmation.val()) {
-              password_confirmation[0].setCustomValidity("Passwords must match.");
-          } else {
-              password_confirmation[0].setCustomValidity("");
-          }
-      };
+          var is_submit_step = function() {
+            return visible_step.hasClass("submit");
+          };
 
-      password.change(checkPasswordValidity);
-      password_confirmation.change(checkPasswordValidity);
+          var is_last_step = function() {
+            return (visible_step[0] == sign_up_wizard(".step").last()[0]);
+          };
 
-      var register_validity_handlers = function() {
-        $(":input").on("invalid", function() {
-          if ($(this).closest(".step")[0] != current_step[0]) {
-            return false; // don't validate hidden inputs
-          } else {
-            $(this).addClass("invalid");
-          }
-        }).on("change", function() {
-          $(this)[0].setCustomValidity("");
+          // update button texts
+          var update_buttons = function() {
+            sign_up_wizard("#back").toggle(!is_first_step() && !is_last_step());
 
-          if ($(this)[0].validity.valid) {
-            $(this).removeClass("invalid");
-          }
+            if (is_submit_step()) {
+              sign_up_wizard("#next").val("Done!");
+            } else if (is_last_step()) {
+              sign_up_wizard("#next").val("Close");
+            } else {
+              sign_up_wizard("#next").val("Next");
+            }
+          };
+
+          var update_step_counter = function() {
+            if (is_last_step()) {
+              sign_up_wizard("#step").hide();
+            } else {
+              var steps = sign_up_wizard(".step");
+              sign_up_wizard("#step").html(1 + steps.index(visible_step) + "/" + (steps.length - 1));
+            }
+          };
+
+          var change_step = function(direction) {
+            var step = sign_up_wizard(".step:visible").hide();
+            step = (direction == 1 ? step.next() : step.prev());
+
+            step.show(0, function() {
+              current_step = step;
+            });
+
+            visible_step = step;
+
+            update_step_counter();
+            update_buttons();
+          };
+
+          // register navigation handlers
+          sign_up_wizard("#next").click(function() {
+            if (is_last_step()) {
+              sign_up_wizard("").remove();
+              return false;
+            }
+
+            var valid = true;
+
+            sign_up_wizard(".step:visible input").each(function() {
+              if (!this.checkValidity()) {
+                valid = false;
+                return false;
+              }
+            });
+
+            if (valid) {
+              if (is_submit_step()) {
+                var form = sign_up_wizard('form');
+
+                $.ajax({
+                  type: "POST",
+                  url: host + "/sign_up_wizard/sign_up",
+                  crossDomain: true,
+                  xhrFields: {
+                    withCredentials: true
+                  },
+                  data: form.serialize(),
+                  beforeSend: function() {
+                    sign_up_wizard("footer input").attr("disabled", true);
+                    sign_up_wizard("#next").val("...");
+
+                    sign_up_wizard("#message").hide();
+                  },
+                  complete: function() {
+                    sign_up_wizard("footer input").attr("disabled", false);
+                  },
+                  success: function(response) {
+                    change_step(1);
+                  },
+                  error: function(response) {
+                    update_buttons();
+                    update_step_counter();
+
+                    sign_up_wizard("#message").html(response.responseText).show();
+                  }
+                });
+              } else {
+                change_step(1);
+              }
+            }
+            // else render HTML5 validation messages
+          });
+
+          sign_up_wizard("#back").click(function() {
+            change_step(-1);
+            sign_up_wizard("#message").hide();
+          });
+
+          // register validation handlers
+          var password = sign_up_wizard("#distributor_password");
+          var password_confirmation = sign_up_wizard("#distributor_password_confirmation");
+
+          var check_password_validity = function() {
+            if (password.val() != password_confirmation.val()) {
+                password_confirmation[0].setCustomValidity("Passwords must match.");
+            } else {
+                password_confirmation[0].setCustomValidity("");
+            }
+          };
+
+          password.change(check_password_validity);
+          password_confirmation.change(check_password_validity);
+
+          var register_validity_handlers = function() {
+            sign_up_wizard(":input").on("invalid", function() {
+              if ($(this).closest(".step")[0] != current_step[0]) {
+                console.log("Dont validate ", this.id);
+                return false; // don't validate hidden inputs
+              } else {
+                $(this).addClass("invalid");
+              }
+            }).on("change", function() {
+              // $(this)[0].setCustomValidity("");
+
+              if ($(this)[0].validity.valid) {
+                $(this).removeClass("invalid");
+              }
+            });
+          };
+
+          register_validity_handlers();
+
+          console.log("READY");
         });
-      };
-
-      register_validity_handlers();
-
-    }).appendTo("body");
-
+      }
+    });
   };
-
 };
 
 var _old_bucky_box_sign_up_wizard = window._bucky_box_sign_up_wizard || [];
