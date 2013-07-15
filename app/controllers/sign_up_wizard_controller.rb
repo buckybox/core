0c0d044f20b0fc66b2d647d5047c4b4d565e9a3a
Cédric Félizard <cedric@felizard.fr>
2013-07-15 06:41:52 +0000

    Don't need that

--- app/controllers/sign_up_wizard_controller.rb
+++ app/controllers/sign_up_wizard_controller.rb
@@ -94,10 +94,6 @@ private
   def set_cors_headers
     if request.headers["HTTP_ORIGIN"]
       headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
-      headers['Access-Control-Expose-Headers'] = 'ETag'
-      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
-      headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
-      headers['Access-Control-Max-Age'] = '86400'
       headers['Access-Control-Allow-Credentials'] = 'true'
     end
   end
