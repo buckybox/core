a85ef9f3ddeba8ff1b43a30a58fa5392fcb4fee8
Samson Ootoovak <sam@enspiral.com>
2013-07-19 15:26:19 +1200

    Forgot to save before a commit.

--- spec/support/devise/request_helpers.rb
+++ spec/support/devise/request_helpers.rb
@@ -17,6 +17,16 @@ module Devise::RequestHelpers
     click_button 'Sign in'
   end
 
+  def sign_in_as_a_valid_customer(customer = nil)
+    @customer = customer || Fabricate(:customer)
+    login_customer @customer
+  end
+
+  def sign_in_as_a_valid_distributor(distributor = nil)
+    @distributor = distributor || Fabricate(:distributor_with_everything)
+    login_distributor @distributor
+  end
+
   def login_customer(customer)
     @last_login = :customer
     login_as customer, scope: :customer
