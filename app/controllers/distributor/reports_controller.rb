c094675b971366698017f8517df00e4cb2f2b899
Jordan Carter <jordan.d.carter@gmail.com>
2013-07-25 17:21:50 +1200

    style changes, remove tracking until it is in master

--- app/controllers/distributor/reports_controller.rb
+++ app/controllers/distributor/reports_controller.rb
@@ -7,7 +7,8 @@ class Distributor::ReportsController < ApplicationController
     csv_string = current_distributor.transaction_history_report(date_from, date_to)
     filename = "bucky-box-transaction-history-export-#{date_from.to_s(:transaction)}-to-#{date_to.to_s(:transaction)}"
 
-    tracking.event(current_distributor, "exported_transaction_history")
+    #Add back when wizard merged to master
+    #tracking.event(current_distributor, "exported_transaction_history")
 
     send_csv(filename, csv_string)
   end
@@ -16,7 +17,8 @@ class Distributor::ReportsController < ApplicationController
     date = Date.parse(params[:to])
     csv_string = CustomerAccountHistoryCsv.generate(date, current_distributor)
 
-    tracking.event(current_distributor, "exported_customer_account_history")
+    #Add back when wizard merged to master
+    #tracking.event(current_distributor, "exported_customer_account_history")
 
     send_csv("bucky-box-customer-account-balance-export-#{date.iso8601}", csv_string)
   end
