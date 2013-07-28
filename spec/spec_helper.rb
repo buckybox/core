37a4b5121aa81fd914300f92b7f6a6f35133b84a
Samson Ootoovak <sam@enspiral.com>
2013-07-28 15:14:59 +1200

    Remove as one was causing an error and one was redundant given settings in config/devise.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,23 +47,4 @@ RSpec.configure do |config|
   config.after(:each) do
     DatabaseCleaner.clean
   end
-
-  # Don't need passwords in test DB to be secure, but we would like 'em to be
-  # fast -- and the stretches mechanism is intended to make passwords
-  # computationally expensive.
-  module Devise
-    module Models
-      module DatabaseAuthenticatable
-        protected
-
-        def password_digest(password)
-          password
-        end
-      end
-    end
-  end
-
-  Devise.setup do |devise|
-    devise.stretches = 0
-  end
 end
