44454260221e816a6e5ba444a06e221b06dc402c
Samson Ootoovak <sam@enspiral.com>
2013-08-13 12:16:30 +1200

    Class method should have been a constant.

--- app/models/customer_csv.rb
+++ app/models/customer_csv.rb
@@ -4,12 +4,43 @@ require 'singleton'
 class CustomerCSV
   include Singleton
 
-  def self.generate(distributor, customer_ids, fields = default_fields)
+  DEFAULT_FIELDS = [
+    :customer_number,
+    :first_name,
+    :last_name,
+    :email,
+    :last_paid_date,
+    :account_balance,
+    :minimum_balance,
+    :halted?,
+    :discount,
+    :customer_labels,
+    :customer_creation_date,
+    :customer_creation_method,
+    :sign_in_count,
+    :customer_note,
+    :customer_packing_notes,
+    :delivery_service,
+    :address_line_1,
+    :address_line_2,
+    :suburb,
+    :city,
+    :postcode,
+    :delivery_note,
+    :mobile_phone,
+    :home_phone,
+    :work_phone,
+    :active_orders_count,
+    :next_delivery_date,
+    :next_delivery,
+  ].freeze
+
+  def self.generate(distributor, customer_ids, fields = DEFAULT_FIELDS)
     data = get_data(distributor, customer_ids)
     instance.generate(data, fields)
   end
 
-  def generate(customers, fields = CustomerCSV.default_fields)
+  def generate(customers, fields = DEFAULT_FIELDS)
     @fields = fields
     CSV.generate do |csv|
       csv << headers
@@ -24,39 +55,6 @@ private
   def self.get_data(distributor, customer_ids)
     data = distributor.customers_for_export(customer_ids)
     data.decorate
-  end
-
-  def self.default_fields
-    [
-      :customer_number,
-      :first_name,
-      :last_name,
-      :email,
-      :last_paid_date,
-      :account_balance,
-      :minimum_balance,
-      :halted?,
-      :discount,
-      :customer_labels,
-      :customer_creation_date,
-      :customer_creation_method,
-      :sign_in_count,
-      :customer_note,
-      :customer_packing_notes,
-      :delivery_service,
-      :address_line_1,
-      :address_line_2,
-      :suburb,
-      :city,
-      :postcode,
-      :delivery_note,
-      :mobile_phone,
-      :home_phone,
-      :work_phone,
-      :active_orders_count,
-      :next_delivery_date,
-      :next_delivery,
-    ]
   end
 
   def headers
