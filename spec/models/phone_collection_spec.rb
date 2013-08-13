ec0a7eb5bd0a4980d032a9da21b1bf30e8d9cc88
Cédric Félizard <cedric@felizard.fr>
2013-08-13 01:15:41 +0000

    Handle phone numbers properly

--- spec/models/phone_collection_spec.rb
+++ spec/models/phone_collection_spec.rb
@@ -1,4 +1,37 @@
 require 'spec_helper'
 
 describe PhoneCollection do
+  context "without any numbers" do
+    before do
+      @address = double("Address",
+        mobile_phone: nil,
+        home_phone: nil,
+        work_phone: nil,
+      )
+
+      @phone_collection = PhoneCollection.new @address
+    end
+
+    it "defaults to the first number" do
+      expect(@phone_collection.default_number).to eq nil
+      expect(@phone_collection.default_type).to eq "mobile"
+    end
+  end
+
+  context "with a work number" do
+    before do
+      @address = double("Address",
+        mobile_phone: nil,
+        home_phone: nil,
+        work_phone: "007",
+      )
+
+      @phone_collection = PhoneCollection.new @address
+    end
+
+    it "defaults to the first present number" do
+      expect(@phone_collection.default_number).to eq "007"
+      expect(@phone_collection.default_type).to eq "work"
+    end
+  end
 end
