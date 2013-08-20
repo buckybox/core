cd4c39329736522aa435562119b7c361a0936064
Jordan Carter <jordan.d.carter@gmail.com>
2013-08-20 14:19:02 +1200

    fix failing export tests

--- spec/models/customer_account_history_csv_spec.rb
+++ spec/models/customer_account_history_csv_spec.rb
@@ -3,40 +3,39 @@ require 'spec_helper'
 describe CustomerAccountHistoryCsv do
   describe "#generate" do
     
-    before(:all) do
-      @date = 1.week.ago.to_date
+    it "exports customer data into csv" do
+      @date = Date.current - 6.weeks
       @distributor = Fabricate(:distributor)
 
-      @customers = 2.times.collect do |i|
-        customer = Fabricate(:customer, distributor: @distributor, number: i+1, first_name: "C#{i+1}", last_name: "CL#{i+1}", email: "#{i+1}@buckybox.com")
-        6.times do |j|
-          l = j+i
-          date = 6.weeks.ago + j.weeks
-          account = customer.account
-          account.subtract_from_balance(l*40, display_time: date)
-          account.add_to_balance(l*45, display_time: date)
-          account.save!
-        end
-        customer.reload
+      @accounts = 2.times.collect do |i|
+        c = Fabricate(:customer, distributor: @distributor, number: i+1, first_name: "C#{i+1}", last_name: "CL#{i+1}", email: "#{i+1}@buckybox.com")
+        c.account
       end
-    end
 
-    it "exports the header into the csv" do
-      rows = CSV.parse(CustomerAccountHistoryCsv.generate(@date, @distributor))
-      rows.first.should eq ["date", "customer number", "customer first name", "customer last name", "customer email", "customer account balance"]
-    end
+      @accounts[0].subtract_from_balance(20, display_time: @date); @accounts[0].save!
+      @accounts[0].add_to_balance(40, display_time: @date + 1.day); @accounts[0].save!
+      @accounts[0].subtract_from_balance(60, display_time: @date + 2.day); @accounts[0].save!
 
-    it "exports customer data into csv" do
-      rows = CSV.parse(CustomerAccountHistoryCsv.generate(@date, @distributor))
-      rows[1..-1].should eq [[@date.iso8601, "0001", "C1", "CL1", "1@buckybox.com", "50.0"],
-                              [@date.iso8601, "0002", "C2", "CL2", "2@buckybox.com", "75.0"]]
-    end
-    
-    it "exports customer data into csv" do
-      date = @date - 3.weeks
+      @accounts[1].add_to_balance(20, display_time: @date); @accounts[1].save!
+      @accounts[1].subtract_from_balance(40, display_time: @date + 1.day); @accounts[1].save!
+      @accounts[1].add_to_balance(60, display_time: @date + 2.day); @accounts[1].save!
+
+      date = @date
       rows = CSV.parse(CustomerAccountHistoryCsv.generate(date, @distributor))
-      rows[1..-1].should eq [[date.iso8601, "0001", "C1", "CL1", "1@buckybox.com", "5.0"],
-                              [date.iso8601, "0002", "C2", "CL2", "2@buckybox.com", "15.0"]]
+      rows[1..-1].should eq [[date.iso8601, "0001", "C1", "CL1", "1@buckybox.com", "-20.0"],
+                              [date.iso8601, "0002", "C2", "CL2", "2@buckybox.com", "20.0"]]
+
+      date = date + 1.day
+      rows = CSV.parse(CustomerAccountHistoryCsv.generate(date, @distributor))
+      rows[1..-1].should eq [[date.iso8601, "0001", "C1", "CL1", "1@buckybox.com", "20.0"],
+                              [date.iso8601, "0002", "C2", "CL2", "2@buckybox.com", "-20.0"]]
+
+      date = date + 1.day
+      rows = CSV.parse(CustomerAccountHistoryCsv.generate(date, @distributor))
+      rows[1..-1].should eq [[date.iso8601, "0001", "C1", "CL1", "1@buckybox.com", "-40.0"],
+                              [date.iso8601, "0002", "C2", "CL2", "2@buckybox.com", "40.0"]]
+      
+      @distributor.destroy
     end
   end
 end
