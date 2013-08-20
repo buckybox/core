c329ee630b0fa3b0203cb5502f8581ece6f4665b
Cédric Félizard <cedric@felizard.fr>
2013-08-20 00:33:38 +0000

    Don't crash if Airbrake is not available

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,3 +1,5 @@
-Airbrake.configure do |config|
-  config.api_key = 'e4145e68bb8f78c4380f502c3cddc339'
+if defined? Airbrake
+  Airbrake.configure do |config|
+    config.api_key = 'e4145e68bb8f78c4380f502c3cddc339'
+  end
 end
