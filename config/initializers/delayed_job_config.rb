2a4d3a6ab4c534fd8dec77944bd4456fef876b57
Jordan Carter <jordan.d.carter@gmail.com>
2013-08-23 14:00:34 +1200

    intercom integration complete

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,5 +1,6 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.delay_jobs = Rails.env.production? || Rails.env.staging? # Run inline for testing
+Delayed::Worker.max_attempts = 8 # Default setting means jobs will be retried up to 30 days later, not ideal.
 
 require 'delayed-plugins-airbrake'
 Delayed::Worker.plugins << Delayed::Plugins::Airbrake::Plugin
--- lib/bucky/tracking.rb
+++ lib/bucky/tracking.rb
@@ -6,12 +6,11 @@ module Bucky
 
     def event(identity, action_name, occurred_at = Time.now)
       raise TypeError, "Identity cannot be nil" if identity.nil?
-      return unless Rails.env.production?
 
       # tracking
       identity.delay(
         priority: Figaro.env.delayed_job_priority_low
-      ).track(action_name, occurred_at)
+      ).track(action_name, occurred_at, Rails.env)
     end
   end
 end
