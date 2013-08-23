10c0a37c373c244b249f8ccd5370189227cc6e44
Jordan Carter <jordan.d.carter@gmail.com>
2013-08-23 13:46:10 +1200

    add airbrake to delayed_jobs

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,2 +1,5 @@
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.delay_jobs = Rails.env.production? || Rails.env.staging? # Run inline for testing
+
+require 'delayed-plugins-airbrake'
+Delayed::Worker.plugins << Delayed::Plugins::Airbrake::Plugin
