33c62af672d43f6aae0064d6b206dfb21ea26fb6
Samson Ootoovak <sam@enspiral.com>
2013-08-28 14:08:21 +1200

    Add setup indicator and welcome modal.

--- spec/models/distributor/setup_spec.rb
+++ spec/models/distributor/setup_spec.rb
@@ -5,15 +5,39 @@ describe Distributor::Setup do
   let(:distributor)       { double("distributor") }
   let(:distributor_setup) { Distributor::Setup.new(distributor) }
 
-  describe "#done?" do
+  describe "#finished?" do
     it "returns false if everything has been set up" do
       distributor.stub(:routes) { [] }
-      expect(distributor_setup.done?).to be_false
+      expect(distributor_setup.finished?).to be_false
     end
 
     it "returns true if everything has been set up" do
       distributor.stub(:routes) { [ double("route") ] }
-      expect(distributor_setup.done?).to be_true
+      expect(distributor_setup.finished?).to be_true
+    end
+  end
+
+  describe "#progress" do
+    it "returns 0 if nothing is done" do
+      distributor.stub(:routes) { [] }
+      expect(distributor_setup.progress).to eq(0)
+    end
+
+    it "returns 100 if nothing is done" do
+      distributor.stub(:routes) { [ double("route") ] }
+      expect(distributor_setup.progress).to eq(100)
+    end
+  end
+
+  describe "#progress_left" do
+    it "returns 0 if nothing is done" do
+      distributor.stub(:routes) { [] }
+      expect(distributor_setup.progress_left).to eq(100)
+    end
+
+    it "returns 100 if nothing is done" do
+      distributor.stub(:routes) { [ double("route") ] }
+      expect(distributor_setup.progress_left).to eq(0)
     end
   end
 end
