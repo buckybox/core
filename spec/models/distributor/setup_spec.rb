957b7e9060eb2e71a40a869172cdd7691c3b6e74
Samson Ootoovak <sam@enspiral.com>
2013-08-29 17:10:20 +1200

    Add the customers step check.

--- spec/models/distributor/setup_spec.rb
+++ spec/models/distributor/setup_spec.rb
@@ -8,6 +8,7 @@ describe Distributor::Setup do
   before do
     distributor.stub(:delivery_services) { [] }
     distributor.stub(:boxes)             { [] }
+    distributor.stub(:customers)         { [] }
   end
 
   describe "#finished?" do
@@ -18,40 +19,37 @@ describe Distributor::Setup do
     it "returns true if everything has been set up" do
       distributor.stub(:delivery_services) { [ double("delivery_service") ] }
       distributor.stub(:boxes)             { [ double("boxes") ] }
+      distributor.stub(:customers)         { [ double("customers") ] }
       expect(distributor_setup.finished?).to be_true
     end
   end
 
   describe "#progress" do
-    it "returns 0.0 if nothing is done" do
+    it "returns a percentage of progress" do
       expect(distributor_setup.progress).to eql(0.0)
-    end
 
-    it "returns 50.0 if the first step has been done" do
       distributor.stub(:delivery_services) { [ double("delivery_service") ] }
-      expect(distributor_setup.progress).to eql(50.0)
-    end
+      expect(distributor_setup.progress).to eql(33.33333333333333)
 
-    it "returns 100.0 if everything is done" do
-      distributor.stub(:delivery_services) { [ double("delivery_service") ] }
-      distributor.stub(:boxes)             { [ double("boxes") ] }
+      distributor.stub(:boxes) { [ double("boxes") ] }
+      expect(distributor_setup.progress).to eql(66.66666666666666)
+
+      distributor.stub(:customers) { [ double("customers") ] }
       expect(distributor_setup.progress).to eql(100.0)
     end
   end
 
   describe "#progress_left" do
-    it "returns 100.0 if nothing is done" do
+    it "returns a percentage of progress" do
       expect(distributor_setup.progress_left).to eql(100.0)
-    end
 
-    it "returns 50.0 if everything is done" do
       distributor.stub(:delivery_services) { [ double("delivery_service") ] }
-      expect(distributor_setup.progress_left).to eql(50.0)
-    end
+      expect(distributor_setup.progress_left).to eql(66.66666666666667)
 
-    it "returns 0.0 if everything is done" do
-      distributor.stub(:delivery_services) { [ double("delivery_service") ] }
-      distributor.stub(:boxes)             { [ double("boxes") ] }
+      distributor.stub(:boxes) { [ double("boxes") ] }
+      expect(distributor_setup.progress_left).to eql(33.33333333333334)
+
+      distributor.stub(:customers) { [ double("customers") ] }
       expect(distributor_setup.progress_left).to eql(0.0)
     end
   end
