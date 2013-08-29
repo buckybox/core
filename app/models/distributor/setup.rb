271e3162fdbec408df6a631b878fec26f830dab6
Samson Ootoovak <sam@enspiral.com>
2013-08-29 16:31:48 +1200

    Add boxes to step list.

--- app/models/distributor/setup.rb
+++ app/models/distributor/setup.rb
@@ -1,16 +1,21 @@
 class Distributor::Setup
 
+  STEPS = [
+    "delivery_services",
+    "boxes",
+  ].freeze
+
   def initialize(distributor, args = {})
     @distributor = distributor
   end
 
   def finished?
-    has_delivery_services?
+    progress == 100.0
   end
 
   def progress
-    done = has_delivery_services? ? 1.0 : 0.0
-    (done / 1.0 * 100.0)
+    percentage = steps_done / total_steps
+    percentage * 100.0
   end
 
   def progress_left
@@ -21,7 +26,19 @@ private
 
   attr_reader :distributor
 
+  def total_steps
+    STEPS.size.to_f
+  end
+
+  def steps_done
+    STEPS.map { |step| send("has_#{step}?") }.count(true).to_f
+  end
+
   def has_delivery_services?
-    !distributor.delivery_services.empty?
+    !distributor.delivery_services.to_a.empty?
+  end
+
+  def has_boxes?
+    !distributor.boxes.to_a.empty?
   end
 end
