f7bfef112f4576961848b325779a730ec0ec6026
Samson Ootoovak <sam@enspiral.com>
2013-09-03 20:51:05 +1200

    Make tweaks based on feedback.

--- app/controllers/distributor/bank_information_controller.rb
+++ app/controllers/distributor/bank_information_controller.rb
@@ -1,12 +1,23 @@
 class Distributor::BankInformationController < Distributor::BaseController
+  def create
+    bank_information = current_distributor.build_bank_information(params[:webstore_order])
+
+    if bank_information.save
+      tracking.event(current_distributor, "new_bank_information") unless current_admin.present?
+      redirect_to distributor_settings_bank_information_url, notice: "Your payment information was successfully updated."
+    else
+      render "distributor/settings/payments", locals: { payments: bank_information }
+    end
+  end
+
   def update
     bank_information = current_distributor.bank_information
 
     if bank_information.update_attributes(params[:bank_information])
       tracking.event(current_distributor, "new_bank_information") unless current_admin.present?
-      redirect_to distributor_settings_payments_url, notice: 'Your payment information was successfully updated.'
+      redirect_to distributor_settings_payments_url, notice: "Your payment information was successfully updated."
     else
-      render 'distributor/settings/payments', locals: { payments: bank_information }
+      render "distributor/settings/payments", locals: { payments: bank_information }
     end
   end
 end
