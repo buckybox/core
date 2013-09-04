0b8a975b407b0f0105eeab74fc8525e30c67d8aa
Jordan Carter <jordan.d.carter@gmail.com>
2013-09-04 14:56:07 +1200

    more intercom refactoring

--- spec/models/distributor_spec.rb
+++ spec/models/distributor_spec.rb
@@ -490,11 +490,11 @@ describe Distributor do
     end
   end
 
-  context "Communications and tracking" do
+  context "Messaging and tracking" do
     subject { distributor }
-    it { should delegate(:tracking_after_create).to(:tracking) }
-    it { should delegate(:tracking_after_save).to(:tracking) }
-    it { should delegate(:track).to(:tracking) }
+    it { should delegate(:tracking_after_create).to(:messaging) }
+    it { should delegate(:tracking_after_save).to(:messaging) }
+    it { should delegate(:track).to(:messaging) }
   end
 
   describe '#transactions_for_export' do
--- spec/models/messaging/distributor_tracking_spec.rb
+++ spec/models/messaging/distributor_tracking_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe Messaging::Distributor do
   let(:distributor){ double('distributor').as_null_object }
   let(:distributor_tracking){ Messaging::Distributor.new(distributor) }
-  let(:comms_tracking){ Messaging::CommsTracking.instance }
+  let(:messaging){ Messaging::IntercomProxy.instance }
 
   describe '#tracking_after_create' do
-    it 'passes call to CommsTracking' do
+    it 'passes call to IntercomProxy' do
       tracking_data = double("tracking_data")
       distributor_tracking.stub(:tracking_data).and_return(tracking_data)
 
-      comms_tracking.stub(:create_user).and_return(nil)
-      comms_tracking.should_receive(:create_user).with(tracking_data, Rails.env)
+      messaging.stub(:create_user).and_return(nil)
+      messaging.should_receive(:create_user).with(tracking_data, Rails.env)
 
       distributor_tracking.tracking_after_create
     end
@@ -19,8 +19,8 @@ describe Messaging::Distributor do
 
   describe '#tracking_after_save' do
     it 'updates tags' do
-      comms_tracking.stub(:update_tags).and_return(nil)
-      comms_tracking.should_receive(:update_tags)
+      messaging.stub(:update_tags).and_return(nil)
+      messaging.should_receive(:update_tags)
       
       distributor_tracking.tracking_after_save
     end
