466ddddd5bacb0199e7ea900dcfb4595c8f70c85
Cédric Félizard <cedric@felizard.fr>
2013-09-10 05:16:07 +0000

    Merge Australian BSB number with account number

---
+++ db/migrate/20130910033818_remove_bsb_number_from_bank_information.rb
@@ -0,0 +1,22 @@
+class RemoveBsbNumberFromBankInformation < ActiveRecord::Migration
+  def up
+    BankInformation.all.each do |bank_information|
+      sanitised_bsb_number = sanitise_number(bank_information.bsb_number)
+      sanitised_account_number = sanitise_number(bank_information.account_number)
+      new_account_number = [sanitised_bsb_number, sanitised_account_number].join
+
+      bank_information.account_number = new_account_number
+      bank_information.save!(validate: false)
+    end
+
+    remove_column :bank_information, :bsb_number
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+
+  def sanitise_number(string)
+    string.to_s.gsub(/[^0-9]/, '')
+  end
+end
