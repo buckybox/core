adab3486521fbd2259bcfcdc8f4398b09f3ddaf5
Samson Ootoovak <sam@enspiral.com>
2013-09-25 15:17:28 +1200

    Refacotr Distributor Defaults to not relay on so many class methods.

--- spec/models/distributor/defaults_spec.rb
+++ spec/models/distributor/defaults_spec.rb
@@ -2,17 +2,33 @@ require "spec_helper"
 
 describe Distributor::Defaults do
 
-  describe ".populate_defaults" do
-    before do
-      @distributor = Fabricate(:distributor,
-        contact_name: "Bob"
-      )
+  let(:distributor) { Fabricate(:distributor, contact_name: "Bob") }
 
-      Distributor::Defaults.populate_defaults(@distributor)
+  describe ".populate_defaults" do
+    before { Distributor::Defaults.populate_defaults(distributor) }
+
+    it "creates the default line items for a distributor" do
+      expect(distributor.line_items).to_not be_empty
     end
 
-    specify { expect(@distributor.line_items).to_not be_empty }
-    specify { expect(@distributor.email_templates).to_not be_empty }
+    it "creates the default email templates for a distributor" do
+      expect(distributor.email_templates).to_not be_empty
+    end
+  end
+
+  describe "#populate_defaults" do
+    before do
+      distributor_defaults = Distributor::Defaults.new(distributor)
+      distributor_defaults.populate_defaults
+    end
+
+    it "creates the default line items for a distributor" do
+      expect(distributor.line_items).to_not be_empty
+    end
+
+    it "creates the default email templates for a distributor" do
+      expect(distributor.email_templates).to_not be_empty
+    end
   end
 
 end
