22042764bdfd0a0e783c292fe8e4e39dbf0055b9
Samson Ootoovak <sam@enspiral.com>
2013-09-25 15:54:27 +1200

    Remove test duplication.

--- spec/models/distributor/defaults_spec.rb
+++ spec/models/distributor/defaults_spec.rb
@@ -4,9 +4,7 @@ describe Distributor::Defaults do
 
   let(:distributor) { Fabricate(:distributor, contact_name: "Bob") }
 
-  describe ".populate_defaults" do
-    before { Distributor::Defaults.populate_defaults(distributor) }
-
+  shared_examples_for "a distributor with populated defaults" do
     it "creates the default line items for a distributor" do
       expect(distributor.line_items).to_not be_empty
     end
@@ -16,19 +14,19 @@ describe Distributor::Defaults do
     end
   end
 
+  describe ".populate_defaults" do
+    before { Distributor::Defaults.populate_defaults(distributor) }
+
+    it_behaves_like "a distributor with populated defaults"
+  end
+
   describe "#populate_defaults" do
     before do
       distributor_defaults = Distributor::Defaults.new(distributor)
       distributor_defaults.populate_defaults
     end
 
-    it "creates the default line items for a distributor" do
-      expect(distributor.line_items).to_not be_empty
-    end
-
-    it "creates the default email templates for a distributor" do
-      expect(distributor.email_templates).to_not be_empty
-    end
+    it_behaves_like "a distributor with populated defaults"
   end
 
 end
