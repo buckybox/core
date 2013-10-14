7da4bece055713f334b8c5914f351f0edcb9e541
Cédric Félizard <cedric@felizard.fr>
2013-10-14 01:07:12 +0000

    Move code for dropdowns into helpers

---
+++ app/helpers/distributor/extras_helper.rb
@@ -0,0 +1,17 @@
+module Distributor::ExtrasHelper
+  def extras_limit_collection
+    1.upto(10).each_with_object({}) do |i, h|
+      h["Allow up to #{pluralize(i, "item")}"] = i
+    end.merge!("Allow any number of items" => 0) # eek!
+  end
+
+  def box_items_limit_collection
+    1.upto(7).each_with_object({}) do |i, h|
+      h["limited to #{pluralize(i, "item")}"] = i
+    end.merge!("without limits" => 0) # eek!
+  end
+
+  def all_extras_collection
+    { "entire catalog" => true, "items below" => false }
+  end
+end
