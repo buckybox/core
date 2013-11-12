80c7f5d98e55a539bad994884273cfee3675bc50
Jdbdean <jdbdean@gmail.com>
2013-11-13 11:24:15 +1300

    Installed API documentation 'apipie'
    reference: https://github.com/Pajk/apipie-rails#api-documentation-tool

--- app/controllers/api/v0/customers_controller.rb
+++ app/controllers/api/v0/customers_controller.rb
@@ -1,14 +1,34 @@
 class Api::V0::CustomersController < Api::V0::BaseController
 	
+	api :GET, '/customers'
+	param :email #, String, "Customer's Email Address" , required: false
 	def index
-    @customers = @distributor.customers
+		cust_email = params[:email]
+		if cust_email.nil?
+    	@customers = @distributor.customers
+    else
+    	@customers = @distributor.customers.find_by(email: cust_email) 
+    end
   end
 
-	def create
-
+  def show
+		cust_id = params[:id]
+		@customer  = @distributor.customers.find_by(id: cust_id)
+		if @customer.nil?
+			not_found
+		end
 	end
 
-  
-
+	def create
+		cust_fname = params[:first_name]
+		cust_lname = params[:last_name]
+		cust_email = params[:email]
+		@customer = Customer.new email: "blah@blah"
+		if @customer.save
+			@customer
+		else
+			internal_server_error customer.errors
+		end
+	end
 
 end
