512a68a31d28d7b527cf818bb20dc6a94a222087
Cédric Félizard <cedric@felizard.fr>
2013-11-15 00:33:30 +0000

    Add more tests for customers endpoint

--- app/controllers/api/v0/customers_controller.rb
+++ app/controllers/api/v0/customers_controller.rb
@@ -1,58 +1,55 @@
 class Api::V0::CustomersController < Api::V0::BaseController
-
-	def_param_group :address_attributes do
-		param :address, Object, "Customer Address", required: true do
-		  param :line_1, String, required: true
-		  param :line_2, String
-		  param :suburb, String
-		  param :city, String
-		  param :postcode, String
-		  param :delivery_note, String
-		  param :mobile_phone, String
-		  param :home_phone, String
-		  param :work_phone, String
-		end
-	end
-
-	def_param_group :customer do
-	  param :customer, Object, "JSON String ", required: true do
-	    param :first_name, String, "First name of the customer", required: true
-	    param :last_name, String, "Last name of the customer"
-	    param :email, String, "Customer's email address", required: true
-	    param_group :address_attributes
-	  end
-
-	end
-
-
-	api :GET, '/customers',  "Get list of customers"
-	param :email , String, desc: "Customer's Email Address. Returns array of single customer"
-	param :embed,String, desc: "Children available: address"
-	example "/customers/?email=will@buckybox.com&embed=address"  
-	def index
-		cust_email = params[:email]
-		if cust_email.nil?
-    	@customers = @distributor.customers
-    else
-    	@customers = @distributor.customers.find_by(email: cust_email) 
+  def_param_group :address_attributes do
+    param :address, Object, "Customer Address", required: true do
+      param :line_1, String, required: true
+      param :line_2, String
+      param :suburb, String
+      param :city, String
+      param :postcode, String
+      param :delivery_note, String
+      param :mobile_phone, String
+      param :home_phone, String
+      param :work_phone, String
     end
   end
 
-	api :GET, '/customers/:id',  "Get single customer"
-	param :id,Integer, desc: "Customer ID", required: true
-	param :embed,String, desc: "Children available: address"
-	example "/customers/123?embed=address"  
-  def show
-		cust_id = params[:id]
-		@customer  = @distributor.customers.find_by(id: cust_id)
-		if @customer.nil?
-			not_found
-		end
-	end
+  def_param_group :customer do
+    param :customer, Object, "JSON String ", required: true do
+      param :first_name, String, "First name of the customer", required: true
+      param :last_name, String, "Last name of the customer"
+      param :email, String, "Customer's email address", required: true
+      param_group :address_attributes
+    end
+  end
 
-	api :POST, '/customers',  "Create a new customer"
-	description 'A json object representing the new customer'
-	example '{ 
+  api :GET, '/customers',  "Get list of customers"
+  param :email, String, desc: "Customer's Email Address. Returns array of single customer"
+  param :embed, String, desc: "Children available: address"
+  example "/customers/?email=will@buckybox.com&embed=address"
+  def index
+    cust_email = params[:email]
+    if cust_email.nil?
+      @customers = @distributor.customers
+    else
+      @customers = @distributor.customers.find_by(email: cust_email)
+    end
+  end
+
+  api :GET, '/customers/:id',  "Get single customer"
+  param :id, Integer, desc: "Customer ID", required: true
+  param :embed, String, desc: "Children available: address"
+  example "/customers/123?embed=address"
+  def show
+    cust_id = params[:id]
+    @customer = @distributor.customers.find_by(id: cust_id)
+    if @customer.nil?
+      not_found
+    end
+  end
+
+  api :POST, '/customers',  "Create a new customer"
+  description 'A json object representing the new customer'
+  example '{
     "customer": {
         "first_name": "Will",
         "last_name": "Lau",
@@ -71,21 +68,21 @@ class Api::V0::CustomersController < Api::V0::BaseController
     }
 }'
 
-	param_group :customer
-	def create
-		new_customer = params[:customer]
-		internal_server_error if new_customer.nil?
+  param_group :customer
+  def create
+    new_customer = params[:customer]
+    internal_server_error if new_customer.nil?
 
-		new_customer = JSON.parse new_customer
-		@customer = Customer.new	new_customer['customer']
-		@customer.distributor_id = @distributor.id
-		@customer.number = Customer.next_number(@distributor)
+    new_customer = JSON.parse new_customer
+    @customer = Customer.new  new_customer['customer']
+    @customer.distributor_id = @distributor.id
+    @customer.number = Customer.next_number(@distributor)
 
-		if @customer.save
-			render 'api/v0/customers/create', status: :created, location: api_v0_customer_url(id: @customer.id) and return
-		else
-			internal_server_error @customer.errors
-		end
-	end
+    if @customer.save
+      render 'api/v0/customers/create', status: :created, location: api_v0_customer_url(id: @customer.id) and return
+    else
+      internal_server_error @customer.errors
+    end
+  end
 
 end
--- spec/requests/api/v0/customers_spec.rb
+++ spec/requests/api/v0/customers_spec.rb
@@ -2,30 +2,65 @@ require "spec_helper"
 
 describe "API v0" do
   describe "customers" do
+    before do
+      distributor.generate_api_key!
+      @customers ||= Fabricate.times(2, :customer, distributor: distributor)
+    end
+
     let(:api_url) { "http://api.test.dev/v0" }
     let(:headers) do
       {
-        "key" => distributor.api_key, # FIXME change to "X-Key" or sth
+        "key" => distributor.api_key, # FIXME change to "X-Key" or sth standard
         "secret" => distributor.api_secret, # FIXME
       }
     end
 
     let(:distributor) { Fabricate(:distributor) }
-
-    before do
-      distributor.generate_api_key!
-    end
+    let(:expected_attributes) { %w(id first_name last_name email delivery_service_id) }
 
     describe "GET /customers" do
       before do
-        @customers = Fabricate.times(2, :customer, distributor: distributor)
-
         get "#{api_url}/customers", nil, headers
+        expect(response).to be_success
       end
 
       it "returns the list of customers" do
-        expect(response).to be_success
         expect(json.size).to eq @customers.size
+      end
+
+      it "returns the expected attributes" do
+        customer = json.first["customer"]
+
+        expect(customer.keys).to eq expected_attributes
+      end
+
+      it "accepts an email as a search query" do
+        customer = @customers.last
+
+        get "#{api_url}/customers?email=#{customer.email}", nil, headers
+        expect(response).to be_success
+        expect(json.size).to eq 1
+        expect(json.first["customer"]["id"]).to eq customer.id
+      end
+    end
+
+    describe "GET /customers/:id" do
+      let(:customer) { @customers.first }
+
+      before do
+        get "#{api_url}/customers/#{customer.id}", nil, headers
+        expect(response).to be_success
+      end
+
+      it "returns the customer" do
+        expect(json.size).to eq 1
+        expect(json["customer"]["id"]).to eq customer.id
+      end
+
+      it "returns the expected attributes" do
+        customer = json["customer"]
+
+        expect(customer.keys).to eq expected_attributes
       end
     end
   end
