c9779a00afa4ba9396ff79e6fd0969ec19083b78
Cédric Félizard <cedric@felizard.fr>
2013-11-18 04:19:12 +0000

    Add tests for delivery services API

--- app/controllers/api/v0/delivery_services_controller.rb
+++ app/controllers/api/v0/delivery_services_controller.rb
@@ -1,7 +1,7 @@
 class Api::V0::DeliveryServicesController < Api::V0::BaseController
-	api :GET, '/delivery_services',  "Get list of delivery services"
-	example "v0/delivery_services"  
-	def index
-		@delivery_services = @distributor.delivery_services
+  api :GET, '/delivery_services',  "Get list of delivery services"
+  example "v0/delivery_services"
+  def index
+    @delivery_services = @distributor.delivery_services
   end
 end
