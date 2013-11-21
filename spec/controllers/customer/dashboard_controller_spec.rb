3c916a7fa3654ba81b152ca1a93e69fafd8fc75f
Cédric Félizard <cedric@felizard.fr>
2013-11-21 00:29:29 +0000

    Fix random failure - distributor needs a bank information here

--- spec/controllers/customer/dashboard_controller_spec.rb
+++ spec/controllers/customer/dashboard_controller_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 
-describe Customer::DashboardController do
-  sign_in_as_customer
+describe Customer::DashboardController, :js do
+  before do
+    @distributor = Fabricate(:distributor_with_information)
+    @customer = Fabricate(:customer, distributor: distributor)
+  end
 
   describe "GET 'index'" do
+    sign_in_as_customer
+
     it "returns http success" do
       get 'index'
       response.should be_success
