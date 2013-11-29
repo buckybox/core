1f27c234dd6dd68b3eb94ab0515002168c9e018e
Cédric Félizard <cedric@felizard.fr>
2013-11-29 01:42:39 +0000

    Set up NTP server

--- config/install.rb
+++ config/install.rb
@@ -24,6 +24,7 @@ require File.expand_path('../helper', __FILE__)
 policy :myapp, :roles => :app do
   requires :build_essential
   requires :htop
+  requires :ntp
   requires :git
   requires :ruby
   requires :ruby_tuned
