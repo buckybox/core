618b85b9d41816b13b1a9c406f253d98541ad300
Cédric Félizard <cedric@felizard.fr>
2013-12-02 10:48:58 +0000

    Update BankAccountNumber to not chew account numbers

--- spec/fabricators/bank_fabricator.rb
+++ spec/fabricators/bank_fabricator.rb
@@ -2,6 +2,6 @@ Fabricator(:bank_information) do
   distributor
   name 'Bank Name'
   account_name 'Account Name'
-  account_number 1
+  account_number '389002002540202'
   customer_message 'Message'
 end
