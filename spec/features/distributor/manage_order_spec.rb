f30508563fc50377c438b19415b14505471158fe
Cédric Félizard <cedric@felizard.fr>
2013-12-04 22:46:27 +0000

    Don't crash when distributor has no extras

--- spec/features/distributor/manage_order_spec.rb
+++ spec/features/distributor/manage_order_spec.rb
@@ -1,16 +1,34 @@
 require "spec_helper"
 
 describe "Create an order for a customer", js: true do
-  it "allows to create a new order" do
-    @distributor = Fabricate(:distributor_with_everything)
-    simulate_distributor_sign_in
+  context "without extras" do
+    it "allows to create a new order" do
+      @distributor = Fabricate(:distributor)
+      simulate_distributor_sign_in
 
-    customer = @distributor.customers.first
+      customer = Fabricate(:customer, distributor: @distributor)
+      Fabricate(:box, distributor: @distributor)
 
-    visit distributor_customer_path(id: customer.id)
-    click_link "Create a new order"
-    page.should have_content "Extras"
-    click_button "Create Order"
-    page.should have_content "Order was successfully created"
+      visit distributor_customer_path(id: customer.id)
+      click_link "Create a new order"
+      page.should_not have_content "Extras"
+      click_button "Create Order"
+      page.should have_content "Order was successfully created"
+    end
+  end
+
+  context "with extras" do
+    it "allows to create a new order" do
+      @distributor = Fabricate(:distributor_with_everything)
+      simulate_distributor_sign_in
+
+      customer = @distributor.customers.first
+
+      visit distributor_customer_path(id: customer.id)
+      click_link "Create a new order"
+      page.should have_content "Extras"
+      click_button "Create Order"
+      page.should have_content "Order was successfully created"
+    end
   end
 end
