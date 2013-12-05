19cb735a57266ac68881b2d72bbe4b002726b32e
Cédric Félizard <cedric@felizard.fr>
2013-12-05 01:33:03 +0000

    Use RSpec DSL for features

--- spec/features/distributor/manage_order_spec.rb
+++ spec/features/distributor/manage_order_spec.rb
@@ -1,34 +1,30 @@
 require "spec_helper"
 
-describe "Create an order for a customer", js: true do
-  context "without extras" do
-    it "allows to create a new order" do
-      @distributor = Fabricate(:distributor)
-      simulate_distributor_sign_in
+feature "Create an order for a customer", js: true do
+  scenario "creates an order without extras" do
+    @distributor = Fabricate(:distributor)
+    simulate_distributor_sign_in
 
-      customer = Fabricate(:customer, distributor: @distributor)
-      Fabricate(:box, distributor: @distributor)
+    customer = Fabricate(:customer, distributor: @distributor)
+    Fabricate(:box, distributor: @distributor)
 
-      visit distributor_customer_path(id: customer.id)
-      click_link "Create a new order"
-      page.should_not have_content "Extras"
-      click_button "Create Order"
-      page.should have_content "Order was successfully created"
-    end
+    visit distributor_customer_path(id: customer.id)
+    click_link "Create a new order"
+    page.should_not have_content "Extras"
+    click_button "Create Order"
+    page.should have_content "Order was successfully created"
   end
 
-  context "with extras" do
-    it "allows to create a new order" do
-      @distributor = Fabricate(:distributor_with_everything)
-      simulate_distributor_sign_in
+  scenario "creates an order with extras" do
+    @distributor = Fabricate(:distributor_with_everything)
+    simulate_distributor_sign_in
 
-      customer = @distributor.customers.first
+    customer = @distributor.customers.first
 
-      visit distributor_customer_path(id: customer.id)
-      click_link "Create a new order"
-      page.should have_content "Extras"
-      click_button "Create Order"
-      page.should have_content "Order was successfully created"
-    end
+    visit distributor_customer_path(id: customer.id)
+    click_link "Create a new order"
+    page.should have_content "Extras"
+    click_button "Create Order"
+    page.should have_content "Order was successfully created"
   end
 end
