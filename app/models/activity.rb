d5afde61acbe511d92c594fec59ea6250af38c61
Cédric Félizard <cedric@felizard.fr>
2013-12-16 00:54:43 +0000

    Add hooks for resuming orders

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -24,9 +24,29 @@ private
       "#{params[:initiator]} paused their order of #{params[:order].box.name} starting #{params[:order].pause_date.strftime("%a %d %b")}"
     },
     order_remove_pause: ->(params) {
-      "#{params[:initiator]} unpaused their order of <em>#{params[:order].box.name}</em>"
+      "#{params[:initiator]} unpaused their order of #{params[:order].box.name}"
     },
-
+    order_resume: ->(params) {
+      "#{params[:initiator]} updated their order of #{params[:order].box.name} to resume on #{params[:order].resume_date.strftime("%a %d %b")}"
+    },
+    order_remove_resume: ->(params) {
+      "#{params[:initiator]} updated their order of #{params[:order].box.name} to no longer resume"
+    },
+    order_update_extras: ->(params) {
+      "#{params[:initiator]} changed the extras for their order of #{params[:order].box.name}"
+    },
+    order_remove_extras: ->(params) {
+      "#{params[:initiator]} no longer wants extras for their order of #{params[:order].box.name}"
+    },
+    order_add_extras: ->(params) {
+      "#{params[:initiator]} added some extras for their order of #{params[:order].box.name}"
+    },
+    order_update_box: ->(params) {
+      "#{params[:initiator]} changed their order from #{params[:old_box_name]} to a #{params[:order].box.name}"
+    },
+    order_update_frequency: ->(params) {
+      "#{params[:initiator]} changed the frequency of #{params[:order].box.name} from #{params[:old_frequency]} to #{params[:order].schedule_rule.frequency}"
+    },
   }
 end
 
--- spec/features/customer/manage_order_spec.rb
+++ spec/features/customer/manage_order_spec.rb
@@ -9,4 +9,39 @@ feature "Manage an order", js: true do
     visit customer_root_path
     page.should have_content @customer.name
   end
+
+  scenario "sets a pause and resume date for an order" do
+    @distributor = Fabricate(:distributor)
+    @customer = Fabricate(:customer, distributor: @distributor)
+    order = Fabricate(:order, customer: @customer)
+
+    # customer facing UI
+    simulate_customer_sign_in
+    visit customer_root_path
+
+    click_link "pause"
+    click_button "Pause"
+    page.should have_content "pausing starts"
+
+    click_link "until further notice"
+    click_button "Resume"
+    page.should have_content "resuming deliveries on"
+
+    find(".pause > .resulting-link > a").click
+    click_link "remove"
+    page.should have_link "pause"
+    page.should_not have_content "pausing starts"
+    page.should_not have_content "resuming deliveries on"
+
+
+    # distributor facing UI
+    simulate_distributor_sign_in
+    visit distributor_customer_path(id: @customer.id)
+    @customer.should have(3).activities
+    page.should have_content "RECENT ACTIVITY"
+    page.should have_content "Less than a minute ago - #{@customer.name} paused their order of #{order.box.name}"
+    page.should have_content "Less than a minute ago - #{@customer.name} updated their order of #{order.box.name} to resume on"
+    page.should have_content "Less than a minute ago - #{@customer.name} unpaused their order of #{order.box.name}"
+  end
 end
+
