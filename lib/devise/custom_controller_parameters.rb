c9eaf1d9c681f7e674edb5729eaa0c9c23aeb630
Cédric Félizard <cedric@felizard.fr>
2014-01-09 10:06:38 +0000

    Keep distributor param in forms

--- lib/devise/custom_controller_parameters.rb
+++ lib/devise/custom_controller_parameters.rb
@@ -9,18 +9,33 @@ module Devise::CustomControllerParameters
     @distributor = Distributor.find_by_parameter_name(params[:distributor]) if params[:distributor]
 
     if @distributor.nil? && params[:customer] && params[:customer][:email]
-      customer = Customer.find_by_email(params[:customer][:email])
-      @distributor = customer.distributor unless customer.nil?
+      customers = Customer.where(email: params[:customer][:email])
+
+      if customers.one?
+        @distributor = customers.first.distributor
+
+      elsif customers.count > 1
+        message = "You must use one of these links:<ul><li>" <<
+          customers.map do |customer|
+            distributor = customer.distributor
+
+            view_context.link_to distributor.name, new_customer_session_url(distributor: distributor.parameter_name)
+          end.join("<li>") << "</ul>"
+
+        flash.now[:alert] = message.html_safe
+      end
     end
 
     if @distributor
       @abort_url = webstore_store_path(@distributor.parameter_name)
-      @abort_text = 'go to webstore'
+      @abort_text = "go to web store"
       @link_args = { distributor: @distributor.parameter_name }
     else
       @abort_url = Figaro.env.marketing_site_url
       @abort_text = "go to #{Figaro.env.marketing_site_url}"
       @link_args = nil
     end
+
+    @distributor_id = @distributor ? @distributor.id : nil
   end
 end
