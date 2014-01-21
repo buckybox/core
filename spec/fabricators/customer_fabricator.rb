e73faa021a806c777aa6fddd4c4e4b8a099772af
Samson Ootoovak <sam@enspiral.com>
2014-01-21 14:29:55 +1300

    Add a customer fabricator with a completed address.

--- spec/fabricators/customer_fabricator.rb
+++ spec/fabricators/customer_fabricator.rb
@@ -5,9 +5,20 @@ Fabricator(:customer) do
   email { sequence(:email) { |i| "customer#{i}@example.com" } }
   password 'password'
   password_confirmation { |attrs| attrs[:password] }
+
+  after_build do |customer|
+    Fabricate(:address, customer: customer)
+  end
 end
 
-Fabricator(:customer_with_address, from: :customer) do
+Fabricator(:customer_with_address, class_name: :customer) do
+  distributor { Fabricate(:distributor_with_information) }
+  delivery_service { |attrs| Fabricate(:delivery_service, distributor: attrs[:distributor]) }
+  first_name { sequence(:first_name) { |i| "First Name #{i}" } }
+  email { sequence(:email) { |i| "customer#{i}@example.com" } }
+  password 'password'
+  password_confirmation { |attrs| attrs[:password] }
+
   after_build do |customer|
     Fabricate(:full_address, customer: customer)
   end
