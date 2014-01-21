cb72cda4e54674c5638e5856c4bcd5b4bd229bd9
Samson Ootoovak <sam@enspiral.com>
2014-01-21 21:55:41 +1300

    Add some integration testing to the distributor side of the customer forms.

--- features/step_definitions/for_customers/contact_and_delivery_form_steps.rb
+++ features/step_definitions/for_customers/contact_and_delivery_form_steps.rb
@@ -18,10 +18,42 @@ Given(/^I am a customer for a distributor that requires (.*?)$/) do |attribute|
   customer_login_with_distributor(distributor)
 end
 
+Given(/^I am a distributor that does not collect (.*?)$/) do |attribute|
+  login_as Fabricate(:distributor_with_everything, collect_options(attribute, false))
+end
+
+Given(/^I am a distributor that collects (.*?)$/) do |attribute|
+  login_as Fabricate(:distributor_with_everything, collect_options(attribute))
+end
+
+Given(/^I am a distributor that does not require (.*?)$/) do |attribute|
+  login_as Fabricate(:distributor_with_everything, require_options(attribute, false))
+end
+
+Given(/^I am a distributor that requires (.*?)$/) do |attribute|
+  login_as Fabricate(:distributor_with_everything, require_options(attribute))
+end
+
+Given(/^I am viewing a customers contact details form$/) do
+  visit distributor_customers_path
+  click_on "0001"
+  click_on "edit profile"
+end
+
+Given(/^I am viewing a customers delivery details form$/) do
+  visit distributor_customers_path
+  click_on "0001"
+  click_on "edit delivery details"
+end
+
 When(/^I submit the form without a phone number$/) do
   pending "Works when changing an existing phone number but not a new one. Fix with form object."
   fill_in "Mobile phone", with: ""
   fill_in "Home phone", with: ""
   fill_in "Work phone", with: ""
   step 'I click on "Update"'
+end
+
+Given(/^PENDING: /)do
+  pending
 end
