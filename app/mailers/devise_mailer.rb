2f65390bd04599520cb69cd269af2dd1ab60c315
Cédric Félizard <cedric@felizard.fr>
2014-01-27 09:49:24 +0000

    Fix prod bug with "lost password" link as a distributor

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -2,10 +2,16 @@ class DeviseMailer < Devise::Mailer
 protected
 
   def headers_for(action, opts)
+    distributor = if resource.respond_to?(:distributor)
+      resource.distributor
+    else
+      resource
+    end
+
     opts.merge!(
       'X-Mailer' => Figaro.env.x_mailer,
-      'From'     => resource.distributor.support_email,
-      'Reply-To' => resource.distributor.support_email,
+      'From'     => distributor.support_email,
+      'Reply-To' => distributor.support_email,
     )
 
     super(action, opts)
