6f90e66b2f62547db60072fc42be4e0ea61d1dac
Cédric Félizard <cedric@felizard.fr>
2014-02-16 07:20:44 +0000

    Make sure the demo distributor is in the list

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,6 +5,9 @@ class Admin::DashboardController < Admin::BaseController
       select { |d| d.transactional_customer_count > 10}.
       sort_by(&:transactional_customer_count).reverse
 
+    demo = Distributor.find_by(email: "demo@buckybox.com")
+    @distributors = [demo] | @distributors if demo
+
     @cron_logs = CronLog.limit(10)
   end
 end
