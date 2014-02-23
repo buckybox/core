e609ee3e3768e3d1af1f0de44effb6c63b7c7233
Cédric Félizard <cedric@felizard.fr>
2014-02-23 07:32:46 +0000

    Upgrade Passenger

--- config/packages/passenger.rb
+++ config/packages/passenger.rb
@@ -1,17 +1,17 @@
 package :passenger, :provides => :appserver do
   description 'Phusion Passenger (mod_rails)'
-  version '3.0.13'
+  version '4.0.37'
   binaries = %w(passenger-config passenger-install-nginx-module passenger-install-apache2-module passenger-make-enterprisey passenger-memory-stats passenger-status)
-  
-  gem 'passenger', :version => version do    
+
+  gem 'passenger', :version => version do
     # Install nginx and the module
     #binaries.each {|bin| post :install, "ln -s usr/local/ruby/bin/#{bin} /usr/local/bin/#{bin}"}
     post :install, "sudo passenger-install-nginx-module --auto --auto-download --prefix=/usr/local/nginx"
     post :install, "echo ** Nginx installed by passenger gem **"
   end
-  
+
   requires :ruby, :passenger_requirements
-  
+
   verify do
     has_gem "passenger", version
     binaries.each {|bin| has_file "/usr/local/bin/#{bin}" }
@@ -20,7 +20,7 @@ end
 
 package :passenger_requirements do
   apt 'libcurl4-openssl-dev'
-  
+
   verify do
     has_apt 'libcurl4-openssl-dev'
   end
