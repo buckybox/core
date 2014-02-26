7aaccd5cac854ffb11e98a30b5d2a7f2c0f7fe07
Cédric Félizard <cedric@felizard.fr>
2014-02-26 00:40:12 +0000

    Inherit settings from production config file

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,94 +1,12 @@
+load Rails.root.join("config/environments/production.rb")
+
 BuckyBox::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.smtp_settings[:domain] = 'staging.buckybox.com'
 
-  # Code is not reloaded between requests
-  config.cache_classes = true
-
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-  
-  #config.cache_store = :redis_store
-
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
-
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
-
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
-
-  # Generate digests for assets URLs
-  config.assets.digest = true
-
-  # Defaults to Rails.root.join("public/assets")
-  # config.assets.manifest = YOUR_PATH
-
-  # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
-
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  require 'hodel_3000_compliant_logger'
-  config.logger = Hodel3000CompliantLogger.new(config.paths['log'].first)
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Disable delivery errors, bad email addresses will be ignored
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: Figaro.env.host }
-
-  # postmark settings
-  config.action_mailer.delivery_method   = :smtp
-  config.action_mailer.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 587,
-    :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => Figaro.env.mandrill_email,
-    :password  => Figaro.env.mandrill_password,
-    :authentication => 'login', # Mandrill supports 'plain' or 'login'
-    :domain => 'staging.buckybox.com', # your domain to identify your server when connecting
-  }
-  
   # ActiveMerchant setup as test only
   config.after_initialize do
     ActiveMerchant::Billing::Base.mode = :test
   end
-
-  # Enable threaded mode
-  # config.threadsafe!
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
-
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  # Oink
-  config.middleware.use(Oink::Middleware)
 end
-
-BuckyBox::Application.middleware.use( Oink::Middleware, :logger => Rails.logger )
 
 
