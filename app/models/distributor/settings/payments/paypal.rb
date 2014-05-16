f1cc10dd363b486ca4381bc30b382ebe419bb1aa
Cédric Félizard <cedric@felizard.fr>
2014-05-16 00:06:16 +0000

    Display error when trying to disable PayPal as the last payment option left

--- app/models/distributor/settings/payments/paypal.rb
+++ app/models/distributor/settings/payments/paypal.rb
@@ -21,5 +21,9 @@ class Distributor::Settings::Payments::Paypal < Distributor::Settings::Payments:
     distributor.omni_importers = new_omni_importers
     distributor.save
   end
+
+  def errors
+    distributor.errors.values.flatten
+  end
 end
 
