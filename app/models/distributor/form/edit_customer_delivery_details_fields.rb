e052b5820b43babf0d1dfb0e0d0a03cc692af894
Cédric Félizard <cedric@felizard.fr>
2014-06-10 13:33:50 +0000

    Set ID instead of resource

--- app/models/distributor/form/edit_customer_delivery_details_fields.rb
+++ app/models/distributor/form/edit_customer_delivery_details_fields.rb
@@ -8,13 +8,13 @@ module Distributor::Form::EditCustomerDeliveryDetailsFields
 
   included do
 
-    attribute :delivery_service,  Integer
-    attribute :address_1,         String
-    attribute :address_2,         String
-    attribute :suburb,            String
-    attribute :city,              String
-    attribute :postcode,          String
-    attribute :delivery_note,     String
+    attribute :delivery_service_id, Integer
+    attribute :address_1,           String
+    attribute :address_2,           String
+    attribute :suburb,              String
+    attribute :city,                String
+    attribute :postcode,            String
+    attribute :delivery_note,       String
 
     def_delegators :distributor,
       :require_address_1?,
@@ -26,7 +26,7 @@ module Distributor::Form::EditCustomerDeliveryDetailsFields
       :require_delivery_note?,
       :delivery_services
 
-    validates_presence_of :delivery_service
+    validates_presence_of :delivery_service_id
     validates_presence_of :address_1,      if: -> { require_address_1? }
     validates_presence_of :address_2,      if: -> { require_address_2? }
     validates_presence_of :suburb,         if: -> { require_suburb? }
@@ -37,18 +37,18 @@ module Distributor::Form::EditCustomerDeliveryDetailsFields
   protected
 
     def assign_attributes(attributes)
-      @delivery_service = attributes["delivery_service"] || customer.delivery_service
-      @address_1        = attributes["address_1"]        || address.address_1
-      @address_2        = attributes["address_2"]        || address.address_2
-      @suburb           = attributes["suburb"]           || address.suburb
-      @city             = attributes["city"]             || address.city
-      @postcode         = attributes["postcode"]         || address.postcode
-      @delivery_note    = attributes["delivery_note"]    || address.delivery_note
+      @delivery_service_id = attributes["delivery_service_id"] || customer.delivery_service_id
+      @address_1           = attributes["address_1"]           || address.address_1
+      @address_2           = attributes["address_2"]           || address.address_2
+      @suburb              = attributes["suburb"]              || address.suburb
+      @city                = attributes["city"]                || address.city
+      @postcode            = attributes["postcode"]            || address.postcode
+      @delivery_note       = attributes["delivery_note"]       || address.delivery_note
     end
 
     def delivery_details_customer_args
       {
-        delivery_service_id:  delivery_service,
+        delivery_service_id:  delivery_service_id,
       }
     end
 
