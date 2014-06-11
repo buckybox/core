367c3eb71125ea8f0b4b4c7766badf4369b89713
Cédric Félizard <cedric@felizard.fr>
2014-06-11 14:02:34 +0000

    Move address validations to concern and use it for distributor form

--- app/models/customer/form/update_delivery_address.rb
+++ app/models/customer/form/update_delivery_address.rb
@@ -2,23 +2,10 @@ require_relative "../form"
 
 class Customer::Form::UpdateDeliveryAddress < Customer::Form
 
-  attribute :address_1
-  attribute :address_2
-  attribute :suburb
-  attribute :city
-  attribute :postcode
-  attribute :delivery_note
+  include Customer::AddressValidations
 
   def_delegators :distributor,
     :collect_delivery_note?
-
-  # FIXME move all this to Concern
-  validates_presence_of :address_1,      if: -> { require_address_1 }
-  validates_presence_of :address_2,      if: -> { require_address_2 }
-  validates_presence_of :suburb,         if: -> { require_suburb }
-  validates_presence_of :city,           if: -> { require_city }
-  validates_presence_of :postcode,       if: -> { require_postcode }
-  validates_presence_of :delivery_note,  if: -> { require_delivery_note }
 
   def save
     return false unless valid?
@@ -48,39 +35,6 @@ private
       postcode:       postcode,
       delivery_note:  delivery_note,
     }
-  end
-
-  # FIXME move all this to Concern
-  def delivery_service
-    customer.delivery_service
-  end
-
-  def pickup_point?
-    delivery_service.pickup_point?
-  end
-
-  def require_address_1
-    !pickup_point? && distributor.require_address_1
-  end
-
-  def require_address_2
-    !pickup_point? && distributor.require_address_2
-  end
-
-  def require_suburb
-    !pickup_point? && distributor.require_suburb
-  end
-
-  def require_city
-    !pickup_point? && distributor.require_city
-  end
-
-  def require_postcode
-    !pickup_point? && distributor.require_postcode
-  end
-
-  def require_delivery_note
-    !pickup_point? && distributor.require_delivery_note
   end
 
 end
