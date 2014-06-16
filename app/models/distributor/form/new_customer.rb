1847aefdaf0db395a471f7d41dffa1d8aef3e821
Cédric Félizard <cedric@felizard.fr>
2014-06-16 08:12:16 +0000

    Code tweaks after review

--- app/models/distributor/form/new_customer.rb
+++ app/models/distributor/form/new_customer.rb
@@ -9,12 +9,12 @@ protected
 
   def customer_args
     args = profile_customer_args.merge(delivery_details_customer_args)
-    args.merge({ distributor: distributor })
+    args.merge(distributor: distributor)
   end
 
   def address_args
     args = profile_address_args.merge(delivery_details_address_args)
-    args.merge({ customer: customer })
+    args.merge(customer: customer)
   end
 
 end
