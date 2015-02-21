7a40cc7620866092257cf7e2a0d5fa2e70b01f37
Samson Ootoovak <sam@enspiral.com>
2015-02-22 01:48:27 +1300

    Move private methods into a utility module.

---
+++ app/controllers/distributor/export/utils.rb
@@ -0,0 +1,35 @@
+module Distributor::Export::Utils
+
+  module_function
+
+  def determine_type(args)
+    [:deliveries, :packages, :orders].find { |key| args.key?(key) }
+  end
+
+  def build_the_csv_exporter_constant(type)
+    type_constant = type.to_s.singularize.titleize.constantize
+    "SalesCsv::#{type_constant}Exporter".constantize
+  end
+
+  def build_csv_args(type, args)
+    {
+      distributor:  args[:distributor],
+      ids:          args[type],
+      date:         Date.parse(args[:date]),
+      screen:       args[:screen]
+    }
+  end
+
+  def build_csv(type, args)
+    csv_exporter = build_the_csv_exporter_constant(type)
+    csv_args     = build_csv_args(type, args)
+    csv_exporter.new(csv_args)
+  end
+
+  def get_export(distributor, args)
+    args = args.merge(distributor: distributor)
+    found_type = determine_type(args)
+    build_csv(found_type, args) if found_type
+  end
+
+end
---
+++ spec/controllers/distributor/export/utils_spec.rb
@@ -0,0 +1,88 @@
+require "spec_helper"
+
+describe Distributor::Export::Utils do
+
+  let(:utils) { Distributor::Export::Utils }
+
+  describe ".determine_type" do
+
+    it "determines the delivery key" do
+      hash = { deliveries: [] }
+      expect(utils.determine_type(hash)).to eq(:deliveries)
+    end
+
+    it "determines the package key" do
+      hash = { packages: [] }
+      expect(utils.determine_type(hash)).to eq(:packages)
+    end
+
+    it "determines the order key" do
+      hash = { orders: [] }
+      expect(utils.determine_type(hash)).to eq(:orders)
+    end
+
+    it "determines if no know key exists" do
+      hash = { unknown_type: [] }
+      expect(utils.determine_type(hash)).to eq(nil)
+    end
+
+  end
+
+  describe "CSV export builder" do
+
+    let(:type) { :deliveries }
+    let(:args) do
+      { distributor: "Vegies", deliveries: [1], date: "2015-02-22", screen: true }
+    end
+    let(:expected) do
+      { distributor: "Vegies", ids: [1], date: Date.parse("2015-02-22"), screen: true }
+    end
+
+    describe ".build_the_csv_exporter_constant" do
+
+      it "creates an export constant" do
+        expect(utils.build_the_csv_exporter_constant(type)).to eq(SalesCsv::DeliveryExporter)
+      end
+
+    end
+
+    describe ".build_csv_args" do
+
+      it "creates the csv arguments" do
+        expect(utils.build_csv_args(type, args)).to eq(expected)
+      end
+
+    end
+
+    describe ".build_csv" do
+
+      it "creates a CSV exporter instance" do
+        expect(SalesCsv::DeliveryExporter).to receive(:new).with(expected)
+        utils.build_csv(type, args)
+      end
+
+    end
+
+    describe ".get_export" do
+
+      let(:result)      { double("result") }
+      let(:distributor) { instance_double("Distributor") }
+
+      before do
+        allow(SalesCsv::DeliveryExporter).to receive(:new).and_return(result)
+      end
+
+      it "creates a CSV exporter instance if the right type is provided" do
+        expect(utils.get_export(distributor, args)).to eq(result)
+      end
+
+      it "does not create a CSV exporter instance if the type is wrong" do
+        args.delete(:deliveries)
+        expect(utils.get_export(distributor, args)).to eq(nil)
+      end
+
+    end
+
+  end
+
+end
