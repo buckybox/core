22a621460a2cf14cbc71a2aba832c602d3735897
Samson Ootoovak <sam@enspiral.com>
2015-02-21 20:18:41 +1300

    Add tests for the exclusions substitutions export.

--- spec/controllers/distributor/deliveries_controller_spec.rb
+++ spec/controllers/distributor/deliveries_controller_spec.rb
@@ -99,4 +99,30 @@ describe Distributor::DeliveriesController do
       @post.call
     end
   end
+
+  describe "#export_exclusions_substitutions" do
+    let(:date) { Date.current.to_s(:db) }
+
+    before do
+      @distributor.save!
+    end
+
+    it "downloads a csv" do
+      allow(ExclusionsSubstitutionsCsv).to receive(:generate).and_return("")
+      post :export_exclusions_substitutions, date: date, deliveries: true
+      expect(response.headers['Content-Type']).to eq "text/csv; charset=utf-8; header=present"
+    end
+
+    it "exports customer data into csv" do
+      allow(ExclusionsSubstitutionsCsv).to receive(:generate).and_return("I am the kind of csvs")
+      post :export_exclusions_substitutions, date: date, deliveries: true
+      expect(response.body).to eq "I am the kind of csvs"
+    end
+
+    it "calls ExclusionsSubstitutionsCsv.generate" do
+      allow(ExclusionsSubstitutionsCsv).to receive(:generate)
+      expect(ExclusionsSubstitutionsCsv).to receive(:generate).with(Date.parse(date), [])
+      post :export_exclusions_substitutions, date: date, deliveries: true
+    end
+  end
 end
