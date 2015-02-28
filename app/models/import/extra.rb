d4f10ed19f8acec93b8eec06cfe17f365825c80e
Samson Ootoovak <sam@enspiral.com>
2015-02-28 17:35:28 +1300

    Move the matching code into it's own class.

--- app/models/import/extra.rb
+++ app/models/import/extra.rb
@@ -3,8 +3,6 @@
 
 class Import::Extra
 
-  attr_reader :distributor, :extra, :box
-
   def initialize(args)
     @distributor = args.fetch(:distributor)
     @extra       = args.fetch(:extra)
@@ -12,33 +10,15 @@ class Import::Extra
   end
 
   def find
-    search_extras = []
     found_box = find_box
-    search_extras = get_extras(found_box)
-    matches = get_matches(search_extras)
-    get_match(matches)
+    extras    = get_extras(found_box)
+    matcher   = Import::Extra::Matcher.new(match: extra, from: extras)
+    matcher.closest_match
   end
 
 private
 
-  def get_match(matches)
-    if matches.size > 1 && matches.first.first == matches[1].first
-      # At-least the first two matches have the same fuzzy_match (probably no unit set)
-      # So return the first one alphabetically so that it is consistent
-      matches.select{ |m| m.first == matches.first.first }. #Select those which have the same fuzzy_match
-        collect(&:last). # discard the fuzzy_match number
-        sort_by{|current_extra| "#{current_extra.name} #{current_extra.unit}"}.first # Sort alphabeticaly
-    else
-      matches.first.last if matches.first.present?
-    end
-  end
-
-  def get_matches(search_extras)
-    search_extras.select{|current_extra| current_extra.match_import_extra?(extra)}.
-      collect{|extra_match| [extra_match.fuzzy_match(extra),extra_match]}.
-      select{|fuzzy_match| fuzzy_match.first > Extra::FUZZY_MATCH_THRESHOLD}. # Set a lower threshold which weeds out almost matches and force the data to be fixed.  Make the user go fix the csv file.
-      sort{|a,b| b.first <=> a.first}
-  end
+  attr_reader :distributor, :extra, :box
 
   def get_extras(found_box)
     if found_box.blank?
