1963d48d689580ff96528dcb9a76f288daaed926
Samson Ootoovak <sam@enspiral.com>
2015-03-01 13:39:17 +1300

    Move matches collection finder into it's own class.

--- app/models/import/extra/matcher.rb
+++ app/models/import/extra/matcher.rb
@@ -2,50 +2,39 @@ class Import::Extra::Matcher
 
   def initialize(args)
     @extra   = args.fetch(:match)
-    @extras  = args.fetch(:from)
-    @matches = preprocess
+    @matches = args.fetch(:from)
   end
 
   def closest_match
     if same_two_matches
       matches.select(&same_fuzzy_match).map(&:last).sort_by(&name_and_unit).first
-    elsif matches.first.present?
-      matches.first.last
+    elsif first_match.present?
+      first_match.last
     end
   end
 
 private
 
-  attr_reader :extra, :extras, :matches
-  
-  def preprocess
-    results = extras.select(&first_filter)
-    results = results.map(&fuzzy_match).select(&fuzzy_filter)
-    results.sort { |a,b| b.first <=> a.first }
-  end
+  attr_reader :extra, :matches
 
   def same_two_matches
-    matches.size > 1 && matches[0].first == matches[1].first
+    matches.size > 1 && first_match.first == second_match.first
   end
 
   def same_fuzzy_match
-    Proc.new { |match| match.first == matches.first.first }
+    Proc.new { |match| match.first == first_match.first }
   end
 
   def name_and_unit
     Proc.new { |current_extra| "#{current_extra.name} #{current_extra.unit}" }
   end
 
-  def first_filter
-    Proc.new { |current_extra| current_extra.match_import_extra?(extra) }
+  def first_match
+    @first_match ||= matches.to_a[0]
   end
 
-  def fuzzy_match
-    Proc.new { |extra_match| [ extra_match.fuzzy_match(extra), extra_match ] }
-  end
-
-  def fuzzy_filter
-    Proc.new { |fuzzy_match| fuzzy_match.first > Extra::FUZZY_MATCH_THRESHOLD }
+  def second_match
+    @second_match ||= matches.to_a[0]
   end
 
 end
