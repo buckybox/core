18b3ded272e585d5d6202386d08bd0b2647ac443
Samson Ootoovak <sam@enspiral.com>
2015-03-08 22:11:38 +1300

    Update namespace for image upload

--- app/uploaders/bucky_image_uploader.rb
+++ app/uploaders/bucky_image_uploader.rb
@@ -3,8 +3,7 @@ class BuckyImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  include Sprockets::Helpers::RailsHelper
-  include Sprockets::Helpers::IsolatedHelper
+  include Sprockets::Rails::Helper
 
   # Choose what kind of storage to use for this uploader:
   storage :file
