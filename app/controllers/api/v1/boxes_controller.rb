eadd3d31785e6fd97a9d60d2c2afd42088e59e7e
Cédric Félizard <cedric@felizard.fr>
2015-03-15 00:49:48 +0000

    Don't fetch associations when they're not required

--- app/controllers/api/v1/boxes_controller.rb
+++ app/controllers/api/v1/boxes_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::BoxesController < Api::V1::BaseController
   example '/v1/boxes?embed=extras,images,box_items'
   def index
     @boxes = @distributor.boxes.not_hidden.all
-    fetch_children
+
+    fetch_images if with_images?
   end
 
   api :GET, '/boxes/:id', "Returns single box"
@@ -14,20 +15,22 @@ class Api::V1::BoxesController < Api::V1::BaseController
   def show
     @box = @distributor.boxes.not_hidden.find_by(id: params[:id])
     return not_found if @box.nil?
-    fetch_children
+
+    fetch_images if with_images?
   end
 
 private
 
-  # TODO: don't fetch all those if @embed is false!
-  def fetch_children
-    @items = @distributor.line_items
-
+  def fetch_images
     boxes = @boxes || [@box]
-    @box_images = boxes.each_with_object({}) do |box, hash|
+    @images = boxes.each_with_object({}) do |box, hash|
       hash[box.id] = box.box_image.versions.each_with_object({}) do |(version, image), images|
         images[version] = ["//", Figaro.env.host, view_context.image_path(image.url)].join
       end
     end
+  end
+
+  def with_images?
+    !@embed["images"].nil?
   end
 end
