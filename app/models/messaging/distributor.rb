7c5c0330582ec5f571dfb30430ff73e9e9689e5d
Cédric Félizard <cedric@felizard.fr>
2015-03-23 14:15:49 +0000

    Try to fix custom Intercom attributes

--- app/models/messaging/distributor.rb
+++ app/models/messaging/distributor.rb
@@ -56,13 +56,7 @@ module Messaging
         name:        distributor.contact_name,
         created_at:  distributor.created_at,
         custom_data: {
-          business_name: distributor.name,
-          phone:         distributor.phone,
-          needs_setup:   distributor.needs_setup?,
-          admin_link:    Rails.application.routes.url_helpers.admin_distributor_url(
-            id: distributor.id,
-            host: Figaro.env.host,
-          )
+          # FIXME: stuff here never gets sent to Intercom?
         }
       }
     end
