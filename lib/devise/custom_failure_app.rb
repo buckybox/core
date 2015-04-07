9e7b102f486b92189f891a2c9b0c007c4ccea68d
Cédric Félizard <cedric@felizard.fr>
2015-04-07 14:07:10 +0000

    Log login failures instead of sending emails

--- lib/devise/custom_failure_app.rb
+++ lib/devise/custom_failure_app.rb
@@ -2,29 +2,15 @@ class Devise::CustomFailureApp < Devise::FailureApp
 
   def respond
     if params.key?(:customer)
+      details = params[:customer].dup
+      details[:password_hash] = Digest::SHA1.hexdigest details.delete(:password)
+      CronLog.log("Login failure for #{details[:email]}", details.inspect)
+
       Librato.increment "bucky.customer.sign_in.failure.from_failure_app"
       Librato.increment "bucky.customer.sign_in.failure.total"
-
-      info = params[:customer]
-      info[:password_hash] = Digest::SHA1.hexdigest info.delete(:password)
-
-      send_login_failure_email info
     end
 
     super
-  end
-
-private
-
-  def send_login_failure_email info
-    AdminMailer.delay(
-      priority: Figaro.env.delayed_job_priority_low,
-      queue: "#{__FILE__}:#{__LINE__}",
-    ).information_email(
-      to: "sysadmins@buckybox.com",
-      subject: "Login failure!",
-      body: info.inspect,
-    )
   end
 
 end
