467ab6d949708856655625f66ea02d20b5edda93
Cédric Félizard <cedric@felizard.fr>
2015-04-09 23:55:14 +0000

    Share session cookie with other subdomains
    Used to share sessions with store.buckybox.com

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-BuckyBox::Application.config.session_store :cookie_store, key: '_bucky_box_session'
+BuckyBox::Application.config.session_store :cookie_store, key: '_bucky_box_session', domain: '.buckybox.com'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
