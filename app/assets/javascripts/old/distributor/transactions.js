88b7114e1fbab2715d5f7e8eaddad00fc3fe74d7
Cédric Félizard <cedric@felizard.fr>
2015-04-15 11:23:14 +0000

    Fix "more transactions" link in customer profile

--- app/assets/javascripts/old/distributor/transactions.js
+++ app/assets/javascripts/old/distributor/transactions.js
@@ -38,7 +38,7 @@ $(function(){
     });
     $('#more-transactions a').click(function() {
       $(this).hide();
-      var account_id = $('#account-information').data('account_id');
+      var account_id = $('#account-information').data('account-id');
       var offset = $('#transactions .transaction-data').length;
       $.get("/" + $("#sortable_transactions").data("user") + "/accounts/" + account_id + "/transactions/" + $('#sortable_transactions table tr.transaction-data').size() + "/more", function(data){
         $('#sortable_transactions').html(data);
