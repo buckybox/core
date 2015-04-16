5d70ef2f649195b232f091b677480a7cc15918c2
Cédric Félizard <cedric@felizard.fr>
2015-04-16 21:07:00 +0000

    Simplify geo IP call with Geokit Gem

--- lib/bucky/geolocation.rb
+++ lib/bucky/geolocation.rb
@@ -2,17 +2,15 @@ require "timeout"
 
 module Bucky
   module Geolocation
-    FREE_GEO_IP_URL = "http://freegeoip.net/json/"
-
   module_function
 
     def get_country ip_address
       info = Geolocation.get_geoip_info ip_address
-      info["country_code"] if info
+      info.country_code if info
     end
 
     def get_time_zone country_code
-      country = Country.where(alpha2: country_code).first
+      country = Country.find_by(alpha2: country_code)
       country.time_zone if country
     end
 
@@ -51,17 +49,12 @@ module Bucky
     end
 
     def get_geoip_info ip_address
-      uri = URI.parse([FREE_GEO_IP_URL, ip_address].join)
-      http = Net::HTTP.new(uri.host, uri.port)
-
-      begin
-        Timeout::timeout(1) do
-          response = http.request(Net::HTTP::Get.new(uri.request_uri))
-          JSON.parse(response.body)
-        end
-      rescue Timeout::Error, JSON::ParserError, SocketError, Errno::ECONNREFUSED
-        # too slow or not valid JSON, just return nil
+      Timeout::timeout(1) do
+        info = Geokit::Geocoders::FreeGeoIpGeocoder.do_geocode(ip_address)
+        info.success ? info : nil
       end
+    rescue Timeout::Error # too slow
+      nil
     end
   end
 end
