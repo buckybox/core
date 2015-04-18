77745c1448b5b168f7896a8c248504d4a861a169
Cédric Félizard <cedric@felizard.fr>
2015-04-18 14:42:11 +0000

    Put slow Librato calls through DJ

--- lib/devise/custom_failure_app.rb
+++ lib/devise/custom_failure_app.rb
@@ -6,8 +6,8 @@ class Devise::CustomFailureApp < Devise::FailureApp
       details[:password_hash] = Digest::SHA1.hexdigest details.delete(:password)
       CronLog.log("Login failure for #{details[:email]}", details.inspect)
 
-      Librato.increment "bucky.customer.sign_in.failure.from_failure_app"
-      Librato.increment "bucky.customer.sign_in.failure.total"
+      Librato.increment_async "bucky.customer.sign_in.failure.from_failure_app"
+      Librato.increment_async "bucky.customer.sign_in.failure.total"
     end
 
     super
