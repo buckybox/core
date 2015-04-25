9a11cf655b49daca15796edb730e2b744d93fcfe
Cédric Félizard <cedric@felizard.fr>
2015-04-25 10:29:35 +0000

    Don't crash if delivery service is not found

--- app/controllers/api/v1/delivery_services_controller.rb
+++ app/controllers/api/v1/delivery_services_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::DeliveryServicesController < Api::V1::BaseController
   api :GET, '/delivery_services/:id', "Returns single delivery service"
   example "v1/delivery_services/123"
   def show
-    delivery_service_id = params[:id]
-    @delivery_service = @distributor.delivery_services.find_by(id: delivery_service_id)
+    @delivery_service = @distributor.delivery_services.find_by(id: params[:id])
+    return not_found if @delivery_service.nil?
   end
 end
