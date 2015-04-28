afc34a8cb99333db3388f8b05dcf03820e65ab24
Cédric Félizard <cedric@felizard.fr>
2015-04-28 09:25:16 +0000

    Fix assets compilation detection

--- config/deploy.rb
+++ config/deploy.rb
@@ -154,13 +154,13 @@ namespace :deploy do
         task :precompile, :roles => :web, :except => { :no_release => true } do
           from = source.next_revision(current_revision)
 
-          if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+          if capture("cd #{latest_release} && #{source.local.log(from)} -- vendor/assets/ app/assets/ | wc -l").to_i > 0
             run %{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
           else
             logger.info "Skipping application asset pre-compilation because there were no changes"
           end
 
-          if capture("cd #{latest_release} && #{source.local.log(from)} app/controllers/api/ | wc -l").to_i > 0
+          if capture("cd #{latest_release} && #{source.local.log(from)} -- app/controllers/api/ | wc -l").to_i > 0
             run %{cd #{latest_release} && RAILS_ENV=#{rails_env} #{rake} apipie:static OUT=#{shared_path}/docs/api}
           else
             logger.info "Skipping API doc pre-compilation because there were no changes"
