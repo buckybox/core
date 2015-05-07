6c4b681a836bec070d060ef4599497298e5838c6
Cédric Félizard <cedric@felizard.fr>
2015-05-07 21:00:44 +0000

    Add sales to admin dashboard

--- app/decorators/crazy_money_decorator.rb
+++ app/decorators/crazy_money_decorator.rb
@@ -8,4 +8,17 @@ class CrazyMoneyDecorator < Draper::Decorator
     currency = distributor.try(:currency)
     object.with_currency(currency)
   end
+
+  def to_human
+    if self < 1E3
+      amount, suffix = self, ""
+    else
+      amount, suffix = (self / 1E3), "k"
+    end
+
+    [
+      amount.round.to_s(decimal_places: 0),
+      suffix,
+    ].join
+  end
 end
