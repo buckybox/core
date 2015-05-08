7cf949102afdfdd5ed37bd068e708ade776b2dc4
Cédric Félizard <cedric@felizard.fr>
2015-05-08 02:25:09 +0000

    Remove dead code

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,20 +1,6 @@
 class AdminMailer < ApplicationMailer
   include ERB::Util # to get `html_escape` helper
 
-  def preview_email(email, admin)
-    @email = email
-    @admin = admin
-
-    headers['X-MC-Tags'] = "admin,preview,#{@email.preview_email.parameterize}"
-
-    mail to: @email.preview_email,
-         from: Figaro.env.support_email,
-         subject: @email.subject do |format|
-          format.text { render text: @email.mail_merge(@admin) }
-          format.html { render text: simple_format(@email.mail_merge(@admin)) }
-         end
-  end
-
   def information_email(options)
     headers['X-MC-Tags'] = "admin,information"
 
