f10c0de66ef0526cdbf38cf31fb92b342d4d4296
Samson Ootoovak <sam@enspiral.com>
2015-05-09 16:27:10 +1200

    Start a basic seed file for testing customer list.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "fabrication"
+require "faker"
+
+RANDOM_BALANCE = Random.new
+
+TAG_GROUPS = [
+  ["cbd", "rural", ""],
+  ["new", "beta", ""],
+  ["coupon", "subscription", ""]
+]
+
+def random_tag_list
+  TAG_GROUPS.map{ |tag_group| tag_group.sample }.join(", ")
+end
+
+distributor = Fabricate(:distributor_with_everything, name: "Local Veggie Group", email: "fake@distributor.com")
+
+delivery_services = [
+  Fabricate(:delivery_service, distributor: distributor, name: Faker::Company.name),
+  Fabricate(:delivery_service, distributor: distributor, name: Faker::Company.name),
+  Fabricate(:delivery_service, distributor: distributor, name: Faker::Company.name)
+]
+
+100.times do
+  customer = Fabricate(:customer_with_address,
+                       distributor: distributor,
+                       delivery_service: delivery_services.sample,
+                       first_name: Faker::Name.name
+                      )
+  customer.tag_list = random_tag_list
+  customer.save
+  account = customer.account
+  account.change_balance_to!(RANDOM_BALANCE.rand(-999..999))
+  account.save
+end
