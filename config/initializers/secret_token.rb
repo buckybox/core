0b7697a6e19748d24820b6ccb93362921ed2b354
Cédric Félizard <cedric@felizard.fr>
2015-05-22 19:10:54 +0000

    Fix build

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -8,7 +8,6 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-
 BuckyBox::Application.config.secret_token = if Rails.env.development? || Rails.env.test?
   "c3df8f74b5d59798bb4f5b1b63021d38c09dfaa7af81650d4b744121c1dc2de3753a275f1979976365c5ecfe1b1fa4077abae03a68ef1a4b16bd765927e76c9a"
 else
