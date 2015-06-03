c4625a85f8a1edba5d1448ce99a4565d705fd819
Cédric Félizard <cedric@felizard.fr>
2015-06-03 12:58:01 +0000

    Need to strip commas from sender for Mandrill

--- lib/bucky/email.rb
+++ lib/bucky/email.rb
@@ -2,7 +2,7 @@ module Bucky
   module Email
     # @return Sanitised header usable with Mandrill
     def sanitise_email_header(header)
-      header.gsub ":", ""
+      header.gsub(/[:,]/, "")
     end
   end
 end
