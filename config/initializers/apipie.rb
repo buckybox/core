24969d6800d8aa83fff99d8ce3533be769a79598
Cédric Félizard <cedric@felizard.fr>
2015-06-08 15:49:42 +0000

    Tweak copyright date

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,7 @@
 Apipie.configure do |config|
   config.app_name                = "Bucky Box API"
   config.app_info["v1"]          = "The Bucky Box API is intended for third-party shopping carts and systems to push new customers and orders into Bucky Box. To access you must send your API-Key and API-Secret via HTTP headers. Please contact Bucky Box developer support to purchase an API key."
-  config.copyright               = "&copy; #{Time.now.year} Bucky Box Limited"
+  config.copyright               = "&copy; 2013-#{Time.now.year} Bucky Box Limited"
   config.doc_base_url            = "/docs"
   config.default_version         = "v1"
   config.api_base_url["v1"]      = "/v1"
