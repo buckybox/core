b694171409f8c8330f621ab0e28e7da85db9d3d9
Cédric Félizard <cedric@felizard.fr>
2015-06-08 16:19:06 +0000

    Use double quotes everywhere

--- config/routes.rb
+++ config/routes.rb
@@ -3,31 +3,31 @@ API_SUBDOMAIN = { subdomain: /\A(staging-)?api\Z/ } unless defined? API_SUBDOMAI
 BuckyBox::Application.routes.draw do
   apipie
 
-  devise_for :admins,       controllers: { sessions: 'admin/sessions' }
-  devise_for :distributors, controllers: { sessions: 'distributor/sessions', passwords: 'distributor/passwords' }
-  devise_for :customers,    controllers: { sessions: 'customer/sessions', passwords: 'customer/passwords' }
+  devise_for :admins,       controllers: { sessions: "admin/sessions" }
+  devise_for :distributors, controllers: { sessions: "distributor/sessions", passwords: "distributor/passwords" }
+  devise_for :customers,    controllers: { sessions: "customer/sessions", passwords: "customer/passwords" }
 
   match "/delayed_job" => DelayedJobWeb, anchor: false
-  match '/' => 'api#index', constraints: API_SUBDOMAIN
+  match "/" => "api#index", constraints: API_SUBDOMAIN
 
-  root to: 'distributor/customers#index'
+  root to: "distributor/customers#index"
 
   get "/directory" => "directory#index"
 
   namespace :sign_up_wizard do
-    get 'form'
-    get 'country'
-    post 'sign_up'
+    get "form"
+    get "country"
+    post "sign_up"
   end
 
   namespace :distributor do
-    root to: 'welcome#index'
+    root to: "welcome#index"
 
     namespace :settings do
-      get 'organisation'
-      get 'webstore'
-      post 'webstore', action: 'save_webstore'
-      post 'spend_limit_confirmation'
+      get "organisation"
+      get "webstore"
+      post "webstore", action: "save_webstore"
+      post "spend_limit_confirmation"
 
       resource :delivery_services, only: [:show, :create, :update, :destroy]
 
@@ -45,17 +45,17 @@ BuckyBox::Application.routes.draw do
     end
 
     namespace :notifications do
-      post 'dismiss_all', actions: 'dismiss_all', as: 'dismiss_all'
+      post "dismiss_all", actions: "dismiss_all", as: "dismiss_all"
     end
 
     namespace :intro_tour do
-      post 'dismiss', actions: 'dismiss', as: 'dismiss'
+      post "dismiss", actions: "dismiss", as: "dismiss"
     end
 
     namespace :reports do
-      get '/',                               action: 'index'
-      get 'transaction_history/:start/:to',  action: 'transaction_history',      as: 'transaction_history'
-      get 'customer_account_history/:to',    action: :customer_account_history,  as: 'customer_account_history'
+      get "/",                               action: "index"
+      get "transaction_history/:start/:to",  action: "transaction_history",      as: "transaction_history"
+      get "customer_account_history/:to",    action: :customer_account_history,  as: "customer_account_history"
     end
 
     resources :distributors,        only: :update
@@ -65,7 +65,7 @@ BuckyBox::Application.routes.draw do
 
     resources :line_items, except: [:index, :show, :update] do
       collection do
-        put 'bulk_update', action: :bulk_update, as: 'bulk_update'
+        put "bulk_update", action: :bulk_update, as: "bulk_update"
       end
     end
 
@@ -73,21 +73,21 @@ BuckyBox::Application.routes.draw do
 
     resources :payments, only: [:create, :index, :show, :destroy] do
       collection do
-        get 'upload_transactions', action: 'upload_transactions', as: 'upload_transactions'
-        post 'commit_upload',      action: 'commit_upload',       as: 'commit_upload'
-        post 'create_from_csv',    action: 'create_from_csv',     as: 'create_from_csv'
-        post 'process_upload',     action: 'process_upload',      as: 'process_upload'
-        post 'index',              action: 'match_payments',      as: 'match_payments'
+        get "upload_transactions", action: "upload_transactions", as: "upload_transactions"
+        post "commit_upload",      action: "commit_upload",       as: "commit_upload"
+        post "create_from_csv",    action: "create_from_csv",     as: "create_from_csv"
+        post "process_upload",     action: "process_upload",      as: "process_upload"
+        post "index",              action: "match_payments",      as: "match_payments"
       end
 
       member do
-        put 'process_payments', action: 'process_payments', as: 'process_payments'
+        put "process_payments", action: "process_payments", as: "process_payments"
       end
     end
 
     resources :import_transactions, only: [:update] do
       member do
-        get 'load_more_rows/:position', action: 'load_more_rows', as: 'load_more_rows'
+        get "load_more_rows/:position", action: "load_more_rows", as: "load_more_rows"
       end
     end
 
@@ -95,109 +95,109 @@ BuckyBox::Application.routes.draw do
 
     resources :customers, except: [:edit] do
       collection do
-        get 'search',   action: :index, as: 'search'
-        get 'tag/:tag', action: :index, as: 'tag'
-        post 'email'
-        post 'export'
+        get "search",   action: :index, as: "search"
+        get "tag/:tag", action: :index, as: "tag"
+        post "email"
+        post "export"
       end
 
       member do
         get :send_login_details
-        get 'impersonate'
-        get 'activity'
-        get 'edit_profile',             action: 'edit_profile',             as: 'edit_profile'
-        put 'update_profile',           action: 'update_profile',           as: 'update_profile'
-        get 'edit_delivery_details',    action: 'edit_delivery_details',    as: 'edit_delivery_details'
-        put 'update_delivery_details',  action: 'update_delivery_details',  as: 'update_delivery_details'
+        get "impersonate"
+        get "activity"
+        get "edit_profile",             action: "edit_profile",             as: "edit_profile"
+        put "update_profile",           action: "update_profile",           as: "update_profile"
+        get "edit_delivery_details",    action: "edit_delivery_details",    as: "edit_delivery_details"
+        put "update_delivery_details",  action: "update_delivery_details",  as: "update_delivery_details"
       end
     end
 
     resources :accounts, only: :edit do
       resources :orders, except: [:index, :show, :destroy] do
         member do
-          put 'deactivate'
-          put 'pause'
-          post 'remove_pause'
-          put 'resume'
-          post 'remove_resume'
+          put "deactivate"
+          put "pause"
+          post "remove_pause"
+          put "resume"
+          post "remove_resume"
         end
       end
 
       resources :boxes do
         member do
-          get 'extras'
+          get "extras"
         end
       end
 
       member do
-        put 'change_balance', action: :change_balance, as: 'change_balance'
-        get 'transactions/:limit(/:more)', action: :transactions, as: 'transactions'
-        get 'receive_payment', action: :receive_payment, as: 'receive_payment'
-        post 'save_payment',   action: :save_payment, as: 'save_payment'
+        put "change_balance", action: :change_balance, as: "change_balance"
+        get "transactions/:limit(/:more)", action: :transactions, as: "transactions"
+        get "receive_payment", action: :receive_payment, as: "receive_payment"
+        post "save_payment",   action: :save_payment, as: "save_payment"
       end
     end
   end
 
   namespace :customer do
-    root to: 'dashboard#index'
+    root to: "dashboard#index"
 
-    get 'dashboard',               controller: 'dashboard', action: 'index'
-    get 'order/:order_id/box/:id', controller: 'dashboard', action: 'box'
+    get "dashboard",               controller: "dashboard", action: "index"
+    get "order/:order_id/box/:id", controller: "dashboard", action: "box"
 
-    put 'update_contact_details',   controller: 'customers',  action: 'update_contact_details'
-    put 'update_delivery_address',  controller: 'customers',  action: 'update_delivery_address'
-    put 'update_password',          controller: 'customers',  action: 'update_password'
+    put "update_contact_details",   controller: "customers",  action: "update_contact_details"
+    put "update_delivery_address",  controller: "customers",  action: "update_delivery_address"
+    put "update_password",          controller: "customers",  action: "update_password"
 
     resources :boxes, only: [:show] do
       member do
-        get 'extras'
+        get "extras"
       end
     end
 
     resources :orders, only: [:edit, :update] do
       member do
-        put 'pause'
-        post 'remove_pause'
-        put 'resume'
-        post 'remove_resume'
-        put 'deactivate'
+        put "pause"
+        post "remove_pause"
+        put "resume"
+        post "remove_resume"
+        put "deactivate"
       end
     end
 
     resources :accounts, only: [] do
-      get 'transactions/:limit(/:more)', action: :transactions, as: 'transactions'
+      get "transactions/:limit(/:more)", action: :transactions, as: "transactions"
     end
   end
 
   namespace :admin do
-    root to: 'dashboard#index'
+    root to: "dashboard#index"
 
     resources :cron_logs, only: :index
     resources :style_sheet, only: :index
 
     resources :distributors, only: [:index, :new, :create, :edit, :update] do
       member do
-        get 'impersonate'
+        get "impersonate"
       end
 
       collection do
-        get 'unimpersonate'
-        get 'country_setting/:id', controller: 'distributors', action: 'country_setting'
-        get 'tag/:tag', action: :index, as: 'tag'
+        get "unimpersonate"
+        get "country_setting/:id", controller: "distributors", action: "country_setting"
+        get "tag/:tag", action: :index, as: "tag"
       end
     end
 
     resources :omni_importers do
       member do
-        post 'test'
+        post "test"
       end
     end
   end
 
   namespace :api, path: "", defaults: { format: :json }, constraints: API_SUBDOMAIN do
     namespace :v1 do
-      get 'ping' => "base#ping"
-      post '/customers/sign_in'
+      get "ping" => "base#ping"
+      post "/customers/sign_in"
       resources :customers,         only: [:index, :show, :create, :update]
 
       resources :boxes,             only: [:index, :show]
