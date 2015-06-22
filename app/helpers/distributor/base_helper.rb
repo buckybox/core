f7e4fe34732a757e0eef875e71655887e4b83089
Cédric Félizard <cedric@felizard.fr>
2015-06-22 11:40:41 +0000

    Backport link_to_function for now

--- app/helpers/distributor/base_helper.rb
+++ app/helpers/distributor/base_helper.rb
@@ -45,4 +45,11 @@ module Distributor::BaseHelper
 
     link_to_function name, function, options
   end
+
+  def link_to_function(name, function, html_options = {})
+    onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
+    href = html_options[:href] || '#'
+
+    content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
+  end
 end
