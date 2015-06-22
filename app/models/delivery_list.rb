562e481f94159418080951940d0a4f23697f402e
Cédric Félizard <cedric@felizard.fr>
2015-06-22 10:13:06 +0000

    Replace undefined method in Rails 4

--- app/models/delivery_list.rb
+++ app/models/delivery_list.rb
@@ -30,8 +30,8 @@ class DeliveryList < ActiveRecord::Base
   end
 
   def self.generate_list(distributor, date)
-    packing_list  = PackingList.find_or_create_by_distributor_id_and_date(distributor.id, date)
-    delivery_list = DeliveryList.find_or_create_by_distributor_id_and_date(distributor.id, date)
+    packing_list  = PackingList.find_or_create_by(distributor_id: distributor.id, date: date)
+    delivery_list = DeliveryList.find_or_create_by(distributor_id: distributor.id, date: date)
 
     # Collecting via packing list rather than orders so that delivery generation is explicitly
     # linked with packages.
