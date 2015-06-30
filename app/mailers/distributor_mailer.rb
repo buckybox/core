59b0c151db51bd224417bc7bd215ffc294177aef
Cédric Félizard <cedric@felizard.fr>
2015-06-30 18:30:00 +0000

    Send admin emails with GMail
    Saves Mandrill usage

--- app/mailers/distributor_mailer.rb
+++ app/mailers/distributor_mailer.rb
@@ -2,8 +2,6 @@ class DistributorMailer < ApplicationMailer
   def welcome(distributor)
     @distributor = distributor
 
-    headers['X-MC-Tags'] = "distributor,welcome"
-
     send_via_gmail mail to: @distributor.email_to,
                         from: "Will Lau <#{Figaro.env.support_email}>",
                         subject: "#{@distributor.name}, welcome to Bucky Box!"
@@ -13,25 +11,8 @@ class DistributorMailer < ApplicationMailer
     @distributor = distributor
     @bank_name = bank_name
 
-    headers['X-MC-Tags'] = "distributor,bank_setup"
-
     send_via_gmail mail to: @distributor.email_to,
                         from: "Bucky Box Support <#{Figaro.env.support_email}>",
                         subject: "[Bucky Box] Setting up your bank feed"
-  end
-
-private
-
-  def send_via_gmail(email)
-    email.delivery_method.settings.merge!(
-      address:              Figaro.env.gmail_smtp_host,
-      port:                 Figaro.env.gmail_smtp_port,
-      user_name:            Figaro.env.gmail_smtp_user_name,
-      password:             Figaro.env.gmail_smtp_password,
-      authentication:       Figaro.env.gmail_smtp_authentication,
-      enable_starttls_auto: Figaro.env.gmail_smtp_enable_starttls_auto,
-    )
-
-    email
   end
 end
