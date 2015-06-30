495b2d639c05fa5b49725880fd669124f2dc1e28
Cédric Félizard <cedric@felizard.fr>
2015-06-30 18:30:00 +0000

    Send admin emails with GMail
    Saves Mandrill usage

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -2,11 +2,9 @@ class AdminMailer < ApplicationMailer
   include ERB::Util # to get `html_escape` helper
 
   def information_email(options)
-    headers['X-MC-Tags'] = "admin,information"
-
     body = options.delete :body
 
-    mail({ from: Figaro.env.support_email }.merge(options)) do |format|
+    send_via_gmail mail({ from: Figaro.env.support_email }.merge(options)) do |format|
       format.text { render text: body }
       format.html { render text: simple_format(html_escape(body)) }
     end
