84baf6578d3f79f6b43497dd468e62d4b2c07910
Cédric Félizard <cedric@felizard.fr>
2015-07-02 10:41:54 +0000

    Fix missing body for admin mailer

---
+++ spec/mailers/admin_mailer_spec.rb
@@ -0,0 +1,28 @@
+require "spec_helper"
+
+describe AdminMailer do
+  describe "#information_email" do
+    let(:options) do
+      {
+        to: Figaro.env.sysalerts_email,
+        subject: "Data integrity tests failed [#{Rails.env}]",
+        body: "Doh!",
+      }
+    end
+
+    let(:mail) { AdminMailer.information_email(options) }
+
+    it "has the right headers" do
+      expect(mail.to).to eq [Figaro.env.sysalerts_email]
+      expect(mail.subject).to eq "Data integrity tests failed [test]"
+
+      [/plain/, /html/].each do |type|
+        expect(mail.body.parts.find { |p| p.content_type.match(type) }.body.raw_source).to include "Doh!"
+      end
+    end
+
+    it "uses GMail's SMTP server" do
+      expect(mail.delivery_method.settings[:address]).to eq Figaro.env.gmail_smtp_host
+    end
+  end
+end
