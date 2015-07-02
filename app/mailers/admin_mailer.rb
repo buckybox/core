6d0ddbdecaae604ee03c679086eb3c90fe2300eb
Cédric Félizard <cedric@felizard.fr>
2015-07-02 10:41:54 +0000

    Fix missing body for admin mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,9 +4,11 @@ class AdminMailer < ApplicationMailer
   def information_email(options)
     body = options.delete :body
 
-    send_via_gmail mail({ from: Figaro.env.support_email }.merge(options)) do |format|
+    email = mail({ from: Figaro.env.support_email }.merge(options)) do |format|
       format.text { render text: body }
       format.html { render text: simple_format(html_escape(body)) }
     end
+
+    send_via_gmail email
   end
 end
