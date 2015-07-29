2425da91bb01dc7f214bbfba435f10b8485e5786
Cédric Félizard <cedric@felizard.fr>
2015-07-29 16:40:49 +0000

    Fix crash when no password is present

--- lib/devise/custom_failure_app.rb
+++ lib/devise/custom_failure_app.rb
@@ -2,7 +2,8 @@ class Devise::CustomFailureApp < Devise::FailureApp
   def respond
     if params.key?(:customer)
       details = params[:customer].dup
-      details[:password_hash] = Digest::SHA1.hexdigest details.delete(:password)
+      password = details.delete(:password) || ""
+      details[:password_hash] = Digest::SHA1.hexdigest password
       CronLog.log("Login failure for #{details[:email]}", details.inspect)
 
       Librato.increment "bucky.customer.sign_in.failure.from_failure_app"
