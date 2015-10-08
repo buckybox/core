cd5f703de4b3c8673f1d0741fc56641087deecc0
Cédric Félizard <cedric@felizard.fr>
2015-10-08 18:52:49 +0000

    Allow Bugsnag and GA over SSL

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,8 +6,8 @@ SecureHeaders::Configuration.configure do |config|
   config.csp = {
     enforce: false,
     default_src: "'self'",
-    img_src: "'self' https://www.google-analytics.com/ https://*.pingdom.net/",
-    script_src: "'self' 'unsafe-inline' https://www.google-analytics.com/ https://*.pingdom.net/ https://js-agent.newrelic.com/ https://bam.nr-data.net/",
+    img_src: "'self' https://www.google-analytics.com/ https://ssl.google-analytics.com/ https://*.pingdom.net/",
+    script_src: "'self' 'unsafe-inline' https://www.google-analytics.com/ https://*.pingdom.net/ https://js-agent.newrelic.com/ https://bam.nr-data.net/ https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js",
     style_src: "'self' 'unsafe-inline'",
     form_action: "'self' https://www.paypal.com/",
     frame_ancestors: "'none'",
