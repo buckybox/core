267e7eddce72b5151eea9cdb9be44b53ff299bf4
Cédric Félizard <cedric@felizard.fr>
2015-10-08 19:28:00 +0000

    More CSP tweaking

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -5,11 +5,12 @@ SecureHeaders::Configuration.configure do |config|
   config.x_xss_protection = { value: 1, mode: "block" }
   config.csp = {
     enforce: false,
-    default_src: "'self'",
-    img_src: "'self' *.google-analytics.com *.pingdom.net",
-    script_src: "'self' 'unsafe-inline' www.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js",
+    default_src: "'none'",
+    img_src: "'self' *.google-analytics.com *.pingdom.net js.intercomcdn.com",
+    script_src: "'self' 'unsafe-inline' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net js.intercomcdn.com widget.intercom.io https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js",
     style_src: "'self' 'unsafe-inline'",
     form_action: "'self' www.paypal.com",
+    connect_src: "'self' *.intercom.io wss://*.intercom.io",
     frame_ancestors: "'none'",
     block_all_mixed_content: "",
     report_uri: "https://api.buckybox.com/v1/csp-report",
