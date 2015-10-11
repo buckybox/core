909bd461eda0932ef62deaa8b57fb1c064932107
Cédric Félizard <cedric@felizard.fr>
2015-10-11 21:41:55 +0000

    Allow to load images from intercom.io

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,8 +6,8 @@ SecureHeaders::Configuration.configure do |config|
   config.csp = {
     enforce: false,
     default_src: "'none'",
-    img_src: "'self' *.google-analytics.com *.pingdom.net *.intercomcdn.com",
-    script_src: "'self' 'unsafe-inline' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net js.intercomcdn.com widget.intercom.io https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js",
+    img_src: "'self' *.google-analytics.com *.pingdom.net *.intercomcdn.com *.intercom.io",
+    script_src: "'self' 'unsafe-inline' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net *.intercomcdn.com *.intercom.io https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js",
     style_src: "'self' 'unsafe-inline'",
     form_action: "'self' www.paypal.com",
     connect_src: "'self' *.google-analytics.com *.intercom.io wss://*.intercom.io",
