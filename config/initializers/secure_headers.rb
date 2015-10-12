19e7a64b075265cac98dde6a7cfe6b4e311bcf43
Cédric Félizard <cedric@felizard.fr>
2015-10-12 14:08:16 +0000

    Allow requests for Bugsnag and OSM

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -6,7 +6,7 @@ SecureHeaders::Configuration.configure do |config|
   config.csp = {
     enforce: false,
     default_src: "'none'",
-    img_src: "'self' *.google-analytics.com *.pingdom.net *.intercomcdn.com *.intercom.io",
+    img_src: "'self' *.google-analytics.com *.pingdom.net *.intercomcdn.com *.intercom.io notify.bugsnag.com *.tile.openstreetmap.org",
     script_src: "'self' 'unsafe-inline' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net *.intercomcdn.com *.intercom.io https://d2wy8f7a9ursnm.cloudfront.net/bugsnag-2.min.js",
     style_src: "'self' 'unsafe-inline'",
     form_action: "'self' www.paypal.com",
