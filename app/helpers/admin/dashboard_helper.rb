4b770ba0c5b24d164c00fc3048cdd58287cf3b8c
Cédric Félizard <cedric@felizard.fr>
2015-11-09 20:48:57 +0000

    Display something when no upcoming deliveries

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -2,11 +2,11 @@ module Admin::DashboardHelper
   def distributors_next_delivery_date(distributor)
     next_occurrence = distributor.customers.ordered_by_next_delivery.select(:next_order_occurrence_date).first.try(:next_order_occurrence_date)
     if next_occurrence.blank? || next_occurrence < Date.current
-      nil
+      "None"
     elsif next_occurrence == Date.current
-      'Today'
+      "Today"
     elsif next_occurrence == Date.current.tomorrow
-      'Tomorrow'
+      "Tomorrow"
     elsif next_occurrence && (next_occurrence < (Date.current + 6.days))
       next_occurrence.to_s(:weekday)
     elsif next_occurrence
