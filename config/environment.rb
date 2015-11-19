339b44144c766b22e5a90c076adf567aaca2b9c0
Cédric Félizard <cedric@felizard.fr>
2015-11-19 17:28:29 +0000

    Reorganise assets

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,6 @@ BuckyBox::Application.configure do
   # Precompile additional assets
   config.assets.precompile += %w( admin.js admin.css distributor.js distributor.css customer.js customer.css print.js print.css )
   config.assets.precompile += %w( sign_up_wizard.js sign_up_wizard.css conversion_pipeline_elm.js )
-  config.assets.precompile += %w( leaflet.js leaflet.css )
+  config.assets.precompile += %w( vendor/leaflet.js vendor/leaflet.css )
   config.assets.precompile += %w( vendor/Chart.min.js )
 end
