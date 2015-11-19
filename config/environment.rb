a8fbe2165fd58a921921b5d06930d747df1f8a78
Cédric Félizard <cedric@felizard.fr>
2015-11-19 16:02:54 +0000

    Add new transactional customers graph to metrics

--- config/environment.rb
+++ config/environment.rb
@@ -9,4 +9,5 @@ BuckyBox::Application.configure do
   config.assets.precompile += %w( admin.js admin.css distributor.js distributor.css customer.js customer.css print.js print.css )
   config.assets.precompile += %w( sign_up_wizard.js sign_up_wizard.css conversion_pipeline_elm.js )
   config.assets.precompile += %w( leaflet.js leaflet.css )
+  config.assets.precompile += %w( vendor/Chart.min.js )
 end
