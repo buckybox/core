08d6e779ca54c5efdedc312ca1f89dca208e1304
Cédric Félizard <cedric@felizard.fr>
2016-01-18 14:05:02 +0000

    Update some Gems

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,19 +1,19 @@
 require "secure_headers"
 
-SecureHeaders::Configuration.configure do |config|
+SecureHeaders::Configuration.default do |config|
   config.x_frame_options = "DENY"
-  config.x_xss_protection = { value: 1, mode: "block" }
+  config.x_xss_protection = "1; mode=block"
   config.csp = {
     enforce: true,
-    default_src: "'none'",
-    img_src: "'self' *.google-analytics.com *.pingdom.net *.pingdom.com *.intercomcdn.com *.intercomassets.com *.intercom.io *.tile.openstreetmap.org",
-    script_src: "'self' 'unsafe-inline' 'unsafe-eval' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net *.intercomcdn.com *.intercom.io",
-    style_src: "'self' 'unsafe-inline'",
-    form_action: "'self' www.paypal.com",
-    connect_src: "'self' api.buckybox.com *.google-analytics.com *.intercomcdn.com *.intercom.io wss://*.intercom.io",
-    frame_ancestors: "'none'",
-    block_all_mixed_content: "",
-    report_uri: "https://api.buckybox.com/v1/csp-report",
+    default_src: %w('none'),
+    img_src: %w('self' *.google-analytics.com *.pingdom.net *.pingdom.com *.intercomcdn.com *.intercomassets.com *.intercom.io *.tile.openstreetmap.org),
+    script_src: %w('self' 'unsafe-inline' 'unsafe-eval' *.google-analytics.com *.pingdom.net js-agent.newrelic.com bam.nr-data.net *.intercomcdn.com *.intercom.io),
+    style_src: %w('self' 'unsafe-inline'),
+    form_action: %w('self' www.paypal.com),
+    connect_src: %w('self' api.buckybox.com *.google-analytics.com *.intercomcdn.com *.intercom.io wss://*.intercom.io),
+    frame_ancestors: %w('none'),
+    block_all_mixed_content: '',
+    report_uri: %w(https://api.buckybox.com/v1/csp-report),
   }
   # config.hpkp = {
   # TODO: set up HPKP
