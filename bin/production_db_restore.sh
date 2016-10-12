d10c1bc77ae2eccfc01de7be026be6eeb38f1169
Cédric Félizard <cedric@felizard.fr>
2016-10-12 12:03:16 +0000

    Update script to import DB dump from prod

--- bin/production_db_restore.sh
+++ bin/production_db_restore.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-DUMP=core/tmp/$(date "+%y%m%d-%H%M%S").dump
-ssh buckybox-core -C "su postgres -c \"pg_dump -i -F c -b -v core_production\"" > $DUMP
+DUMP=../tmp/$(date "+%y%m%d-%H%M%S").dump
+ssh buckybox-core -C "pg_dump -F c -v -h core-production.cbqr8ikjw2qk.eu-west-1.rds.amazonaws.com -p 5432 -U buckybox -d core_production" > $DUMP
 dropdb bucky_box_development && createdb bucky_box_development && pg_restore -F c -d bucky_box_development -O $DUMP
 echo Done
