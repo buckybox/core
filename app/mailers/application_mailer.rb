8d18f3e7e6f8385de14c1fea22003719137a95eb
Cédric Félizard <cedric@felizard.fr>
2017-07-19 10:50:11 +0000

    Make sure URL helpers use HTTPS in emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,4 +2,12 @@ class ApplicationMailer < ActionMailer::Base
   include ActionView::Helpers::TextHelper
 
   default 'X-Mailer' => Figaro.env.x_mailer
+
+  def default_url_options
+    @default_url_options ||= if Rails.env.production?
+      { protocol: "https", host: Figaro.env.host }
+    else
+      super
+    end
+  end
 end
