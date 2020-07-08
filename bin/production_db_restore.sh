5105c28a7becdb20b5fe48924ff1d3ffe3592623
Cédric Félizard <cedric@felizard.fr>
2020-07-08 19:44:59 +0000

    Tweak DB import script

--- bin/production_db_restore.sh
+++ bin/production_db_restore.sh
@@ -1,8 +1,11 @@
-#!/bin/bash -eux
+#!/bin/bash
+
+set -euxo pipefail
 
 cd $(dirname $0)
 
 DUMP=../tmp/$(date "+%y%m%d-%H%M%S").dump
 ssh buckybox-core -C "pg_dump -F c -v -h core-production.cbqr8ikjw2qk.eu-west-1.rds.amazonaws.com -p 5432 -U buckybox -d core_production" > $DUMP
-dropdb --if-exists bucky_box_development && createdb bucky_box_development && pg_restore -F c -d bucky_box_development -O $DUMP
-echo Done
+dropdb -h 127.0.0.1 -U buckybox --if-exists bucky_box_development
+createdb -h 127.0.0.1 -U buckybox bucky_box_development
+pg_restore -F c -h 127.0.0.1 -U buckybox -d bucky_box_development --no-owner $DUMP
